2e05b86c1c89bd2ffb7d728bde73e74af04eb6c5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-06 16:14:20 -0400

    reorganize files

--- R/get_dydx_fixest.R
+++ R/get_dydx_fixest.R
@@ -1,4 +1,4 @@
-#' @include get_dydx_glm.R
+#' @include get_dydx_stats.R
 get_dydx.fixest <- get_dydx.glm
 
 get_dydx_se.fixest <- get_dydx_se.glm
--- R/get_dydx_ivreg.R
+++ R/get_dydx_ivreg.R
@@ -1,4 +1,4 @@
-#' @include get_dydx_glm.R
+#' @include get_dydx_stats.R
 get_dydx.ivreg <- get_dydx.glm
 
 get_dydx_se.ivreg <- get_dydx_se.glm
