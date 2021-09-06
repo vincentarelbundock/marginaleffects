487268f7b463f3bcac9454ea0d8b943a075de5ee
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-06 14:55:12 -0400

    glmerMod: partial support (no variance)

--- R/get_dydx_loess.R
+++ R/get_dydx_loess.R
@@ -1,21 +1,6 @@
 #' @include get_dydx_glm.R
 get_dydx.loess <- get_dydx.glm
 
-get_dydx_se.loess <- function(model, 
-                              fitfram, 
-                              variable, 
-                              variance, 
-                              numDeriv_method = "simple", 
-                              ...) {
-    if (!is.null(variance)) {
-        stop("The `variance` argument is not supported for models of class `loess`. The `variance` argument must be `NULL`.")
-    }
-    get_dydx.glm(model = model,
-                 fitfram = fitfram,
-                 variable = variable,
-                 variance = variance,
-                 numDeriv_method = numDeriv_method,
-                 ...)
+get_dydx_se.loess <- function(...) {
+stop("The `variance` argument is not supported for models of class `loess`. The `variance` argument must be `NULL`.")
 }
-
-
