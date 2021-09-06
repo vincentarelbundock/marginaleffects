e39850582686003643163c295d8faaa68cf1dcf1
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-06 11:07:35 -0400

    loess support

--- R/get_dydx_fixest.R
+++ R/get_dydx_fixest.R
@@ -1,3 +1,4 @@
+#' @include get_dydx_glm.R
 get_dydx.fixest <- get_dydx.glm
 
 get_dydx_se.fixest <- get_dydx_se.glm
--- R/get_dydx_lm.R
+++ R/get_dydx_lm.R
@@ -1,3 +1,4 @@
+#' @include get_dydx_glm.R
 get_dydx.lm <- get_dydx.glm
 
 get_dydx_se.lm <- get_dydx_se.glm
