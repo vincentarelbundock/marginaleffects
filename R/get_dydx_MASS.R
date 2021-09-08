69e39d84cef34ba3843066feb9b96f3a65e7f406
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-07 23:10:01 -0400

    polr variance

--- R/get_dydx_MASS.R
+++ R/get_dydx_MASS.R
@@ -1,13 +1,98 @@
 #' @include get_dydx_nnet.R
 #' @rdname get_gradient
-get_gradient.polr <- get_gradient.multinom
+get_gradient.polr <- function(model, 
+                                  fitfram, 
+                                  variable, 
+                                  group_name = NULL,
+                                  prediction_type = "probs",
+                                  numDeriv_method = "simple") {
+    fitfram_tmp <- fitfram
+    inner <- function(x) {
+        fitfram_tmp[[variable]] <- x
+        pred <- stats::predict(model, 
+                               newdata = fitfram_tmp, 
+                               type = prediction_type)
+        # grad() expects a vector output
+        pred <- pred[, group_name, drop = TRUE]
+        return(pred)
+    }
+    out <- numDeriv::grad(func = inner, 
+                          x = fitfram[[variable]], 
+                          method = numDeriv_method)
+    # sometimes weird attributes must be stripped
+    out <- as.numeric(out)
+    return(out)
+}
 
 #' @rdname reset_coefs
 reset_coefs.polr <- reset_coefs.default
 
 #' @rdname get_jacobian
-get_jacobian.polr <- get_jacobian.multinom
+#' @export
+get_jacobian.polr <- function(model, 
+                              fitfram, 
+                              variable, 
+                              variance = NULL, 
+                              group_name = NULL,
+                              prediction_type = "probs",
+                              numDeriv_method = "simple", 
+                              ...) {
+    model_tmp <- model
+    inner <- function(x) {
+        model_tmp <- reset_coefs(model_tmp, x)
+        g <- get_gradient(model = model_tmp, 
+                          fitfram = fitfram, 
+                          variable = variable, 
+                          group_name = group_name,
+                          prediction_type = prediction_type,
+                          numDeriv_method = numDeriv_method)
+        return(g)
+    }
+    J <- numDeriv::jacobian(func = inner, 
+                            x = stats::coef(model),
+                            method = numDeriv_method)
+    J
+}
 
-#' @include get_dydx_nnet.R
+
 #' @rdname get_dydx
-get_dydx.polr <- get_dydx.multinom
+#' @export
+get_dydx.polr <- function(model, 
+                          fitfram, 
+                          variable, 
+                          group_name,
+                          variance, 
+                          prediction_type = "probs",
+                          numDeriv_method = "simple", 
+                          ...) {
+
+    # marginal effects
+    g <- get_gradient(model = model,
+                      fitfram = fitfram,
+                      variable = variable,
+                      group_name = group_name,
+                      prediction_type = prediction_type,
+                      numDeriv_method = numDeriv_method)
+    out <- data.frame(rowid = 1:nrow(fitfram),
+                      group = group_name, 
+                      term = variable, 
+                      dydx = g)
+
+
+    # standard errors
+    if (!is.null(variance)) {
+        J <- get_jacobian(model = model,
+                          fitfram = fitfram,
+                          variable = variable,
+                          group_name = group_name,
+                          prediction_type = prediction_type,
+                          numDeriv_method = numDeriv_method)
+        idx <- grepl(paste0("^", group_name, ":"), colnames(variance))
+        V <- variance[names(stats::coef(model)),
+                      names(stats::coef(model))]
+        out$std.error <- se_from_J_V(J, V)
+    }
+
+    # output
+    return(out)
+}
--- tests/testthat/test-dydx_MASS.R
+++ tests/testthat/test-dydx_MASS.R
@@ -9,7 +9,7 @@ test_that("polr vs. margins", {
     tmp <- data.frame(mtcars)
     tmp$carb <- as.factor(tmp$carb)
     mod <- MASS::polr(carb ~ hp + am + mpg, data = tmp) 
-    res <- meffects(mod, variance = NULL, prediction_type = "probs")
+    res <- meffects(mod, prediction_type = "probs")
     mar <- margins(mod)
 })
 
@@ -17,12 +17,11 @@ test_that("polr vs. Stata", {
     stata <- readRDS(test_path("stata/stata.rds"))[["MASS_polr_01"]]
     dat <- read_dta(test_path("stata/data/MASS_polr_01.dta"))
     mod <- MASS::polr(factor(y) ~ x1 + x2, data = dat)
-    ame <- meffects(mod, 
-                    variance = NULL, 
-                    prediction_type = "probs") %>%
+    ame <- meffects(mod, prediction_type = "probs") %>%
            group_by(group, term) %>%
-           summarize(dydx = mean(dydx)) %>%
+           summarize(dydx = mean(dydx), std.error = mean(std.error)) %>%
            mutate(group = as.numeric(group)) %>%
            inner_join(stata, by = c("group", "term"))
     expect_equal(ame$dydx, ame$dydxstata, tolerance = 0.001)
+    expect_equal(ame$std.error, ame$std.errorstata, tolerance = 0.001)
 })
