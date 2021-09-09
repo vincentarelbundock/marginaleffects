dd79573e5877d4560ad2815aa71c4986c425c64d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-09 12:22:00 -0400

    pkgdown and test fix

--- tests/testthat/test-missing.R
+++ tests/testthat/test-missing.R
@@ -1,9 +1,10 @@
+tmp <- mtcars
+tmp$am <- as.logical(tmp$am)
+for (i in seq_along(tmp)) {
+    tmp[[i]][sample(1:nrow(tmp), 1)] <- NA
+}
+
 test_that("original data with NAs do not pose problems in glm and lm.", {
-    tmp <- mtcars
-    tmp$am <- as.logical(tmp$am)
-    for (i in seq_along(tmp)) {
-        tmp[[i]][sample(1:nrow(tmp), 1)] <- NA
-    }
     mod1 <- lm(hp ~ mpg + drat + wt + factor(gear), data = tmp)
     mod2 <- glm(vs ~ mpg + drat + wt + factor(gear), data = tmp, family = binomial)
     expect_s3_class(tidy(mod1), "data.frame")
