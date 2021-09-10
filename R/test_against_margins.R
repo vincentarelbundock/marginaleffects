1f078a36311a4106acf72bf597a7c2f8f7f37125
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-10 17:52:56 -0400

    bug fix: duplicate variables

--- R/test_against_margins.R
+++ R/test_against_margins.R
@@ -8,6 +8,8 @@ test_against_margins <- function(results,
         all(abs((x - y) / x) < tolerance)
     }
 
+    results$type <- NULL
+
     margins_object <- data.frame(margins_object)
     term_names <- unique(results$term)
 
@@ -16,24 +18,29 @@ test_against_margins <- function(results,
     # dydx
     for (tn in term_names) {
         unknown <- results[results$term == tn, "dydx"]
-        known <- as.numeric(margins_object[, paste0("dydx_", tn)])
-        tmp <- is_equal(known, unknown)
-        if (isFALSE(tmp)) {
-            flag <- FALSE
-            if (isTRUE(verbose)) print(sprintf("dydx: %s", tn))
+        lab <- paste0("dydx_", tn)
+        if (lab %in% colnames(margins_object)) {
+            known <- as.numeric(margins_object[, lab])
+            tmp <- is_equal(known, unknown)
+            if (isFALSE(tmp)) {
+                flag <- FALSE
+                if (isTRUE(verbose)) print(sprintf("dydx: %s", tn))
+            }
         }
-
     }
 
     # std.error
     if ("std.error" %in% colnames(results)) {
         for (tn in term_names) {
-            unknown <- results[results$term == tn, "std.error"]
-            known <- as.numeric(margins_object[, paste0("SE_dydx_", tn)])
-            tmp <- is_equal(known, unknown)
-            if (isFALSE(tmp)) {
-                flag <- FALSE
-                if (isTRUE(verbose)) print(sprintf("se: %s", tn))
+            lab <- paste0("SE_dydx_", tn)
+            if (lab %in% colnames(margins_object)) {
+                unknown <- results[results$term == tn, "std.error"]
+                known <- as.numeric(margins_object[, lab])
+                tmp <- is_equal(known, unknown)
+                if (isFALSE(tmp)) {
+                    flag <- FALSE
+                    if (isTRUE(verbose)) print(sprintf("se: %s", tn))
+                }
             }
         }
     }
