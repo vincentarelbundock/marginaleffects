35cede3d25145c65fd6fd5dc0f1df1f32055bdd8
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-11 09:21:29 -0400

    predicted values added to marginaleffects() output

--- tests/testthat/test-prediction_type.R
+++ tests/testthat/test-prediction_type.R
@@ -6,3 +6,12 @@ test_that("multiple prediction types", {
     mfx <- marginaleffects(model, prediction_type = c("response", "zero"))
     expect_true(all(c("response", "zero") %in% mfx$type))
 })
+
+
+test_that("predicted values are added to the data.frame", {
+    mod <- glm(am ~ mpg * wt, data = mtcars, family = binomial)
+    mfx <- marginaleffects(mod, prediction_type = "response")
+    expect_true("predicted_response" %in% colnames(mfx))
+    mfx <- marginaleffects(mod, prediction_type = c("response", "link"))
+    expect_true(all(c("predicted_response", "predicted_link") %in% colnames(mfx)))
+})
