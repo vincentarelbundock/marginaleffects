e2aff92fe29502ac2369a6fbd7eda6ba2e1de5d9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-11 23:19:57 -0400

    ascii packaging problem

--- data-raw/supported_models.R
+++ data-raw/supported_models.R
@@ -1,2 +1,2 @@
-supported_models <- read.csv("data-raw/supported_models.csv", check.names = FALSE)
+supported_models <- read.csv("data-raw/supported_models.csv", check.names = FALSE, encoding = "utf8")
 usethis::use_data(supported_models, overwrite = TRUE)
