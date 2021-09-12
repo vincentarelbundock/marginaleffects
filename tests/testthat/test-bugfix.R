4eb344bf4e3617f1ad0d84fc7a0e5c2b6a70ec50
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-12 19:44:06 -0400

    emmeans version and dependency for bugfix

--- tests/testthat/test-bugfix.R
+++ tests/testthat/test-bugfix.R
@@ -1,4 +1,6 @@
 test_that("Bug stay dead: Issue 55", {
+    # Error: Argument 1 must have names.
+    # vab: possibly caused by a version of `emmeans` < 1.6.3
     dat <- read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/palmerpenguins/penguins.csv")
     dat$large_penguin <- ifelse(dat$body_mass_g > median(dat$body_mass_g, na.rm = TRUE), 1, 0)
     mod <- glm(large_penguin ~ bill_length_mm + flipper_length_mm + species, 
