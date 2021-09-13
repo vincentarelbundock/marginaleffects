5ff8d21a36ea25119810ad3b10fc260f8345cf1f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-12 21:23:43 -0400

    tests

--- tests/testthat/test-marginalmeans.R
+++ tests/testthat/test-marginalmeans.R
@@ -1,6 +1,6 @@
 test_that("temporary errors until implementation", {
-    tmp$am <- as.logical(tmp$am)
     tmp <- mtcars
+    tmp$am <- as.logical(tmp$am)
     mod <- lm(mpg ~ hp + wt + factor(cyl) + am, data = tmp)
     mm <- marginalmeans(mod, variables = c("am", "cyl"))
     expect_error(marginalmeans(mod, newdata = mtcars), regexp = "not yet")
