be1df0e10d08fba36f22be6d1e4ad1ba2cd5d3c7
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-13 15:04:13 -0400

    support: truncreg

---
+++ tests/testthat/test-pkg-plm.R
@@ -0,0 +1,35 @@
+suppressPackageStartupMessages(library("plm", warn.conflicts = FALSE))
+
+test_that("Baltagi (2013) replications from the plm doc", {
+    # replicates some results from Baltagi (2013), table 3.1
+    data("Grunfeld", package = "plm")
+    pool <- plm(inv ~ value + capital, data = Grunfeld, model = "pooling")
+    mfx <- marginaleffects(pool)
+    expect_false(any(mfx$estimate == 0))
+    expect_false(any(mfx$std.error == 0))
+    
+    # within model are not supported by `predict.plm`
+    wi <- plm(inv ~ value + capital,
+              data = Grunfeld, model = "within", effect = "twoways")
+    expect_error(marginaleffects(wi), regexp = "within")
+    
+    swar <- plm(inv ~ value + capital,
+                data = Grunfeld, model = "random", effect = "twoways")
+    mfx <- marginaleffects(swar)
+    expect_false(any(mfx$estimate == 0))
+    expect_false(any(mfx$std.error == 0))
+    
+    amemiya <- plm(inv ~ value + capital,
+                   data = Grunfeld, model = "random", random.method = "amemiya",
+                   effect = "twoways")
+    mfx <- marginaleffects(amemiya)
+    expect_false(any(mfx$estimate == 0))
+    expect_false(any(mfx$std.error == 0))
+    
+    walhus <- plm(inv ~ value + capital,
+                  data = Grunfeld, model = "random", random.method = "walhus",
+                  effect = "twoways")
+    mfx <- marginaleffects(walhus)
+    expect_false(any(mfx$estimate == 0))
+    expect_false(any(mfx$std.error == 0))
+})
