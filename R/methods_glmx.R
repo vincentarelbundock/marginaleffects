fb32b4535468b5db1b4662675bb6053185748965
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-14 12:31:17 -0400

    support glmx

---
+++ R/methods_glmx.R
@@ -0,0 +1,9 @@
+#' @include set_coef.R
+#' @rdname set_coef
+#' @export
+set_coef.glmx <- function(model, coefs) {
+    out <- model
+    out$coefficients$glm <- coefs[names(out$coefficients$glm)]
+    out$coefficients$extra <- coefs[names(out$coefficients$extra)]
+    return(out)
+}
---
+++ tests/testthat/test-pkg-glmx.R
@@ -0,0 +1,11 @@
+suppressPackageStartupMessages({
+    library("glmx")
+    library("MASS")
+})
+
+test_that("glmx: no validity check", {
+    d <- data.frame(x = runif(200, -1, 1))
+    d$y <- rnbinom(200, mu = exp(0 + 3 * d$x), size = 1)
+    model <- glmx(y ~ x, data = d, family = negative.binomial, xlink = "log", xstart = 0)
+    expect_mfx(model)
+})
