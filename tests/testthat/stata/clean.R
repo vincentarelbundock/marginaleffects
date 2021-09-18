6db5a068c57293a02bcaa0eae489a6fd9a0253d7
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-18 09:10:15 -0400

    stata tests

--- tests/testthat/stata/clean.R
+++ tests/testthat/stata/clean.R
@@ -9,19 +9,19 @@ library(readxl)
 results <- list()
 
 #  stats::glm
-tmp <- read.table("results/stats_glm_01.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/stats_glm_01.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
     head(2)
 results[["stats_glm_01"]] <- tmp
 
 #  stats::lm
-tmp <- read.table("results/stats_lm_01.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/stats_lm_01.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
     head(2)
 results[["stats_lm_01"]] <- tmp
 
 #  MASS::polr 
-tmp <- read.table("results/MASS_polr_01.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/MASS_polr_01.txt"), sep = "\t", skip = 4) |>
     setNames(c("group", "dydxstata_x1", "std.errorstata_x1", "dydxstata_x2", "std.errorstata_x2")) |>
     head(3) |>
     transform(group = 2:4) |>
@@ -35,46 +35,47 @@ tmp <- dcast(tmp, group + term ~ statistic, value.var = "value")
 results[["MASS_polr_01"]] <- tmp
 
 #  ivreg::ivreg
-tmp <- read.table("results/ivreg_ivreg_01.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/ivreg_ivreg_01.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
     head(2)
 results[["ivreg_ivreg_01"]] <- tmp
 
 #  betareg::betareg 
-tmp <- read.table("results/betareg_betareg_01.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/betareg_betareg_01.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
     head(1)
 results[["betareg_betareg_01"]] <- tmp
 
 #  MASS::glm.nb
-tmp <- read.table("results/MASS_glm_nb.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/MASS_glm_nb.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
     head(1)
 results[["mass_glm_nb"]] <- tmp
 
 #  AER::tobit
-tmp <- read.table("results/AER_tobit.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/AER_tobit.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
-    head(1)
+    head(5)
 results[["aer_tobit"]] <- tmp
 
 #  AER::tobit (right-censured)
-tmp <- read.table("results/AER_tobit_right4.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/AER_tobit_right4.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
-    head(1)
+    head(5)
 results[["aer_tobit_right"]] <- tmp
 
 #  estimatr::lm_robust
-tmp <- read.table("results/estimatr_lm_robust.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/estimatr_lm_robust.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
-    head(1)
+    head(3)
 results[["estimatr_lm_robust"]] <- tmp
 
 #  estimatr::iv_robust
-tmp <- read.table("results/estimatr_iv_robust.txt", sep = "\t", skip = 4) |>
+tmp <- read.table(test_path("stata/results/estimatr_iv_robust.txt"), sep = "\t", skip = 4) |>
     setNames(c("term", "dydxstata", "std.errorstata")) |>
-    head(1)
+    head(2)
+tmp$std.errorstata <- as.numeric(gsub("\\(|\\)", "", tmp$std.errorstata))
 results[["estimatr_iv_robust"]] <- tmp
 
 # save
-saveRDS(results, file = "stata.rds")
+saveRDS(results, file = test_path("stata/stata.rds"))
