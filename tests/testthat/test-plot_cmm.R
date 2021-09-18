c9e0d50edd8778d306f8895b12e45ef4de559503
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-18 17:17:16 -0400

    plot_cmm ci

--- tests/testthat/test-plot_cmm.R
+++ tests/testthat/test-plot_cmm.R
@@ -2,6 +2,15 @@ skip_on_ci() # different graphics engine produce different snapshots
 
 test_that("two conditions", {
     mod <- lm(mpg ~ hp * wt * am, data = mtcars)
-    vdiffr::expect_doppelganger("cme plot with 2 conditions",
-                                plot_cmm(mod, condition = c("hp", "wt")))
+    p <- plot_cmm(mod, condition = c("hp", "wt"))
+    vdiffr::expect_doppelganger("cmm plot with 2 conditions", p)
+})
+
+
+test_that("conf.level in plots", {
+    mod <- lm(mpg ~ hp * wt * am, data = mtcars)
+    p1 <- plot_cmm(mod, condition = "hp", conf.level = .99)
+    p2 <- plot_cmm(mod, condition = "hp", conf.level = .4)
+    vdiffr::expect_doppelganger("plot_cmm large ci", p1)
+    vdiffr::expect_doppelganger("plot_cmm small ci", p2)
 })
