458fc09dad69876c0234b62d77b5b5d5f5615d00
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-18 14:01:25 -0400

    tobit stata

--- tests/testthat/test-pkg-AER.R
+++ tests/testthat/test-pkg-AER.R
@@ -3,15 +3,22 @@ requiet("AER")
 
 test_that("tobit: no validity check", {
   data("Affairs", package = "AER")
-  mod <- AER::tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating, data = Affairs)
-  mfx <- marginaleffects(mod)
-  tid <- tidy(mfx)
-  expect_s3_class(tid, "data.frame")
-  expect_equal(nrow(tid), 5)
 
-  mod <- AER::tobit(affairs ~ age + yearsmarried + religiousness + occupation + rating, right = 4, data = Affairs)
-  mfx <- marginaleffects(mod)
-  tid <- tidy(mfx)
-  expect_s3_class(tid, "data.frame")
-  expect_equal(nrow(tid), 5)
+  stata <- readRDS(test_path("stata/stata.rds"))$aer_tobit
+  mod1 <- AER::tobit(
+    affairs ~ age + yearsmarried + religiousness + occupation + rating, 
+    data = Affairs)
+  mfx <- merge(tidy(marginaleffects(mod1)), stata)
+  expect_mfx(mod1)
+  expect_equal(mfx$estimate, mfx$dydxstata)
+  expect_equal(mfx$std.error, mfx$std.errorstata)
+  
+  stata <- readRDS(test_path("stata/stata.rds"))$aer_tobit_right
+  mod2 <- AER::tobit(
+    affairs ~ age + yearsmarried + religiousness + occupation + rating, 
+    right = 4, data = Affairs)
+  mfx <- merge(tidy(marginaleffects(mod2)), stata)
+  expect_mfx(mod2)
+  expect_equal(mfx$estimate, mfx$dydxstata)
+  expect_equal(mfx$std.error, mfx$std.errorstata)
 })
--- tests/testthat/test-pkg-estimatr.R
+++ tests/testthat/test-pkg-estimatr.R
@@ -4,7 +4,9 @@ requiet("estimatr")
 
 test_that("iv_robust vs. stata", {
     data(Kmenta, package = "ivreg")
-    model <- estimatr::iv_robust(Q ~ P + D | D + F + A, data = Kmenta)
+    model <- iv_robust(Q ~ P + D | D + F + A, 
+                       se_type = "stata",
+                       data = Kmenta)
     stata <- readRDS(test_path("stata/stata.rds"))$estimatr_iv_robust
     mfx <- tidy(marginaleffects(model))
     mfx <- merge(mfx, stata)
@@ -14,10 +16,12 @@ test_that("iv_robust vs. stata", {
 
 
 test_that("lm_robust vs. stata", {
-    model <- estimatr::lm_robust(carb ~ wt + factor(cyl), mtcars)
+    model <- lm_robust(carb ~ wt + factor(cyl), 
+                       se_type = "stata",
+                       data = mtcars)
     stata <- readRDS(test_path("stata/stata.rds"))$estimatr_lm_robust
     mfx <- tidy(marginaleffects(model))
     mfx <- merge(mfx, stata)
     expect_equal(mfx$dydx, mfx$dydxstata)
-    expect_equal(mfx$std.error, mfx$std.errorstata, tolerance = .1)
+    expect_equal(mfx$std.error, mfx$std.errorstata, tolerance = .01)
 })
