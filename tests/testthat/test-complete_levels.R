1ebdc9c87f26eeba7360f866623fe8e1f4ca49bc
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-19 12:23:12 -0400

    complete_levels: failing test

---
+++ tests/testthat/test-complete_levels.R
@@ -0,0 +1,15 @@
+requiet("dplyr")
+
+test_that("padding with interactions", {
+    dat <- read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/ggplot2movies/movies.csv") %>%
+           mutate(style = case_when(Action == 1 ~ "Action",
+                                    Comedy == 1 ~ "Comedy",
+                                    Drama == 1 ~ "Drama",
+                                    TRUE ~ "Other"),
+                  style = factor(style),
+                  certified_fresh = rating >= 8) %>%
+           filter(length < 240)
+    mod <- glm(certified_fresh ~ length * style, data = dat, family = binomial)
+    res <- marginalmeans(mod, type = c("response", "link"))
+    expect_false(anyNA(res$style))
+})
