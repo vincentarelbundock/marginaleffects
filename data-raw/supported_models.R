59a0e3e87a9665ed75962c8414d27f5a7b336512
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-09-20 11:24:37 -0400

    readme table

--- data-raw/supported_models.R
+++ data-raw/supported_models.R
@@ -1,2 +1,42 @@
+library("gt")
+
 supported_models <- read.csv("data-raw/supported_models.csv", check.names = FALSE, encoding = "utf8")
 usethis::use_data(supported_models, overwrite = TRUE)
+
+
+# gt table
+tmp <- supported_models
+for (i in nrow(tmp):2) {
+    if (tmp$Package[i] == tmp$Package[i-1]) {
+        tmp$Package[i] <- ""
+    }
+}
+colnames(tmp) <- c("Package", 
+                   "Function", 
+                   "dY/dX", 
+                   "Std. Error", 
+                   "dY/dX ", 
+                   "Std. Error ",
+                   "dY/dX  ",
+                   "Std. Error  ")
+idx_green <- lapply(tmp, function(x) x == TRUE)
+idx_red <- lapply(tmp, function(x) x == FALSE)
+for (i in 3:ncol(tmp)) {
+    tmp[[i]] <- ""
+}
+tab <- gt(tmp) %>%
+  tab_spanner(label = "Support", columns = 3:4) %>%
+  tab_spanner(label = "Validity: margins", columns = 5:6) %>%
+  tab_spanner(label = "Validity: Stata", columns = 7:8) %>%
+  tab_style(style = list(cell_borders(sides = c("left", "right"), color = "black", weight = px(1))),
+            locations = list(cells_body(columns = 3:ncol(tmp)))) %>%
+  tab_style(style = list(cell_borders(sides = c("bottom", "top"), color = "black", weight = px(1))),
+            locations = cells_body())
+for (i in 3:ncol(tmp)) {
+    tab <- tab %>%
+    tab_style(style = cell_fill(color = "#c1e1c1"), 
+              locations = cells_body(columns = i, rows = (idx_green[[i]] == TRUE))) %>%
+    tab_style(style = cell_fill(color = "#ff6961"), 
+              locations = cells_body(columns = i, rows = (idx_red[[i]] == TRUE)))
+}
+gt::gtsave(tab, "man/figures/README-supported_models.png")
