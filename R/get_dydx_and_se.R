5d991739bd17882ead74a6338edb1e11d1b1a137
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-10-08 15:52:38 -0400

    refactor get_dydx_and_se

--- R/get_dydx_and_se.R
+++ R/get_dydx_and_se.R
@@ -1,120 +1,71 @@
-#' Get marginal effects and standard errors (internal function)
-#'
-#' @return A data.frame with term names, group names, marginal effects, and
-#' standard errors.
-#' @param variable A string to identify the variable whose marginal effect to compute.
-#' @param fitfram A data.frame over which to compute marginal effects.
-#' @param group_name String to identify the "group" or "level" of the terms to
-#'   estimate. Groups are often used in models like multinomial logit where each
-#'   level of the response variable is associated to its own set of coefficients.
-#' @inheritParams marginaleffects
-#' @rdname get_dydx_and_se
-#' @keywords internal
-#' @export
-get_dydx_and_se <- function (model, ...) {
-    UseMethod("get_dydx_and_se", model)
-}
-
-#' @rdname get_dydx_and_se
-#' @export
-get_dydx_and_se.default <- function(model, 
-                                    variables, 
-                                    fitfram = insight::get_data(model), 
-                                    vcov = stats::vcov(model), 
-                                    group_name = NULL,
-                                    type = "response",
-                                    numDeriv_method = "simple", 
-                                    ...) {
+get_dydx_and_se <- function(model,
+                            variables,
+                            fitfram = insight::get_data(model),
+                            vcov = stats::vcov(model),
+                            group_name = NULL,
+                            type = "response",
+                            numDeriv_method = "simple",
+                            ...) {
 
     coefs <- get_coef(model)
+    group_names <- get_group_names(model)
 
     if (!is.null(vcov)) {
         vcov <- vcov[names(coefs), names(coefs)]
     }
 
-    get_one_variable <- function(v) {
-        if (is.factor(fitfram[[v]]) || is.logical(fitfram[[v]]) || is.character(fitfram[[v]])) {
-            dydx_fun <- get_dydx_categorical
-        } else {
-            dydx_fun <- get_dydx_continuous
-        }
-
-        # Marginal effects
-        g <- dydx_fun(model = model,
-                      fitfram = fitfram,
-                      variable = v,
-                      group_name = group_name,
-                      type = type,
-                      numDeriv_method = numDeriv_method,
-                      ...)
-
-        one_variable <- list()
-        one_variable[["g"]] <- g
-
-        # Standard errors
-        if (!is.null(vcov)) {
-            inner <- function(x) {
-                model_tmp <- set_coef(model, stats::setNames(x, names(coefs)))
-                g <- dydx_fun(model = model_tmp,
-                              fitfram = fitfram,
-                              variable = v,
-                              group_name = group_name,
-                              type = type,
-                              numDeriv_method = numDeriv_method)
-                return(g$dydx)
-            }
-            J <- numDeriv::jacobian(func = inner, 
-                                    x = coefs,
-                                    method = numDeriv_method)
-            colnames(J) <- names(get_coef(model))
-            J_mean <- stats::aggregate(J, by = list(g$term), mean)
-            row.names(J_mean) <- J_mean[[1]]
-            J_mean[[1]] <- NULL
-            one_variable[["J_mean"]] <- as.matrix(J_mean)
-            one_variable[["J"]] <- J
-        }
-        return(one_variable)
-    }
-
-    g_list <- list()
-    J_list <- list()
+    mfx_list <- list()
     J_mean_list <- list()
-    for (v in variables) {
-        tmp <- get_one_variable(v)
-        if ("g" %in% names(tmp)) {
-            g_list[[v]] <- tmp$g
-        }
-        if ("J" %in% names(tmp)) {
-            J_list[[v]] <- tmp$J
-            J_mean_list[[v]] <- tmp$J_mean
-        } else {
-            J_list[[v]] <- NULL
-            J_mean_list[[v]] <- NULL
+
+    # compute marginal effects and standard errors
+    mfx_list <- list()
+    se_mean_list <- list()
+    for (predt in type) {
+        for (gn in group_names) {
+            J_mean_tmp <- list()
+            for (variable in variables) {
+                mfx <- get_dydx(model = model,
+                                variable = variable,
+                                group_name = gn,
+                                fitfram = fitfram,
+                                type = predt,
+                                numDeriv_method = numDeriv_method)
+                mfx <- get_dydx_se(model = model,
+                                   mfx = mfx,
+                                   vcov = vcov,
+                                   variable = variable,
+                                   group_name = gn,
+                                   fitfram = fitfram,
+                                   type = predt,
+                                   numDeriv_method = numDeriv_method)
+                mfx$type <- predt
+                mfx$group <- gn
+                mfx_list <- c(mfx_list, list(mfx))
+                J_mean_tmp <- c(J_mean_tmp, list(attr(mfx, "J_mean")))
+            }
+
+            if (!is.null(vcov)) {
+                # Standard errors at the mean gradient (this is what `Stata` and `margins` report)
+                J_mean <- do.call("rbind", J_mean_tmp)
+                V <- colSums(t(J_mean %*% vcov) * t(J_mean))
+                tmp <- data.frame("group" = gn, 
+                                  "type" = predt, 
+                                  "term" = names(V), 
+                                  "std.error" = sqrt(V))
+                se_mean_list <- c(se_mean_list, list(tmp))
+            }
         }
     }
 
-    out <- do.call("rbind", g_list)
+    out <- do.call("rbind", mfx_list)
     row.names(out) <- NULL
 
     if (!is.null(vcov)) {
-        # Keep row.names for J and J_mean matrices
-        J <- do.call("rbind", J_list)
-        J_mean <- do.call("rbind", J_mean_list)
-        attr(out, "J") <- J
-        attr(out, "J_mean") <- J_mean
-
-        # Unit-level standard errors are much slower to compute (are they, though?)
-        # Var(dydx) = J Var(beta) J'
-        # computing the full matrix is memory-expensive, and we only need the diagonal
-        # algebra trick: https://stackoverflow.com/a/42569902/342331
-        V <- colSums(t(J %*% vcov) * t(J))
-        out$std.error <- sqrt(V)
-
-        # Standard errors at the mean gradient (this is what `Stata` and `margins` report)
-        V <- colSums(t(J_mean %*% vcov) * t(J_mean))
-        tmp <- data.frame("term" = names(V), "std.error" = sqrt(V))
-        row.names(tmp) <- NULL
-        attr(out, "se_at_mean_gradient") <- tmp
+        se <- do.call("rbind", se_mean_list)
+        if ("group" %in% colnames(se) && all(se$group == "main_marginaleffect")) {
+            se$group <- NULL
+        }
+        attr(out, "se_at_mean_gradient") <- se
     }
 
     return(out)
---
+++ R/get_dydx_se.R
@@ -0,0 +1,56 @@
+get_dydx_se <- function(model,
+                        mfx,
+                        vcov,
+                        variable,
+                        group_name,
+                        fitfram,
+                        type,
+                        numDeriv_method) {
+
+    if (is.null(vcov)) {
+        return(mfx)
+    }
+
+    out <- mfx
+
+    coefs <- get_coef(model)
+
+    if (variable %in% get_categorical(fitfram)) {
+        dydx_fun <- get_dydx_categorical
+    } else {
+        dydx_fun <- get_dydx_continuous
+    }
+
+    inner <- function(x) {
+        model_tmp <- set_coef(model, stats::setNames(x, names(coefs)))
+        g <- dydx_fun(model = model_tmp,
+                      fitfram = fitfram,
+                      variable = variable,
+                      group_name = group_name,
+                      type = type,
+                      numDeriv_method = numDeriv_method)
+        return(g$dydx)
+    }
+
+    J <- numDeriv::jacobian(func = inner,
+                            x = coefs,
+                            method = numDeriv_method)
+    colnames(J) <- names(get_coef(model))
+    J_mean <- stats::aggregate(J, by = list(out$term), mean)
+    row.names(J_mean) <- J_mean[[1]]
+    J_mean[[1]] <- NULL
+    J_mean <- as.matrix(J_mean)
+
+    # Unit-level standard errors are much slower to compute (are they, though?)
+    # Var(dydx) = J Var(beta) J'
+    # computing the full matrix is memory-expensive, and we only need the diagonal
+    # algebra trick: https://stackoverflow.com/a/42569902/342331
+    V <- colSums(t(J %*% vcov) * t(J))
+    out$std.error <- sqrt(V)
+
+    # Keep row.names for J and J_mean matrices
+    attr(out, "J") <- J
+    attr(out, "J_mean") <- J_mean
+
+    return(out)
+}
