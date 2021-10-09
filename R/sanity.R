539a50dc9b7b0e33222a8b1715f246c0b418a3bc
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-10-09 07:52:15 -0400

    support marginaleffects: mgcv

--- R/sanity.R
+++ R/sanity.R
@@ -129,7 +129,8 @@ sanity_vcov <- function(model, vcov) {
 
 
 sanity_predict_vector <- function(pred, model, newdata, type) {
-    if (!isTRUE(checkmate::check_atomic_vector(pred))) {
+    if (!isTRUE(checkmate::check_atomic_vector(pred)) &&
+        !isTRUE(checkmate::check_array(pred, d = 1))) {
         msg <- sprintf(
 '`predict(model, type = "%s")` was called on a model of class `%s`, but this command did not produce the expected outcome: A numeric vector of length %s. This can sometimes happen when users try compute a marginal effect for some models with grouped or multivariate outcome which are not supported yet by `marginaleffects` package. Please consult your modeling package documentation to learn what alternative `type` arguments are accepted by the `predict` method, or file a feature request on Github:  https://github.com/vincentarelbundock/marginaleffects/issues',
         type, class(model)[1], nrow(newdata))
