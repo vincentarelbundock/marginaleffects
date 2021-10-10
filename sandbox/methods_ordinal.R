35404e8dfa239302bfa0d84b72d58afb621a45cb
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-10-10 08:35:59 -0400

    sandbox ordinal

--- sandbox/methods_ordinal.R
+++ sandbox/methods_ordinal.R
@@ -1,27 +1,42 @@
 #' @rdname get_predict
 #' @export
-get_predict.clm <- function(model, 
-                            newdata = insight::get_data(model), 
-                            type = "prob", 
+get_predict.clm <- function(model,
+                            newdata = insight::get_data(model),
+                            type = "response",
+                            group_name = NULL,
                             ...) {
-    # notice: $fit
-    pred <- stats::predict(model, 
-                           newdata = newdata, 
+
+    checkmate::assert_choice(type,
+                             choices = c("response", "prob", "cum.prob", "linear.predictor"))
+
+    if (type == "response") {
+        type <- "prob"
+    }
+
+    pred <- stats::predict(model,
+                           newdata = newdata,
                            type = type)$fit
+    sanity_predict_numeric(pred = pred, model = model, newdata = newdata, type = type)
 
     # numDeriv expects a vector
     if (is.matrix(pred) && (!is.null(group_name) && group_name != "main_marginaleffect")) {
         pred <- pred[, group_name, drop = TRUE]
     }
 
-    sanity_predict_numeric(pred = pred, model = model, newdata = newdata, type = type)
     sanity_predict_vector(pred = pred, model = model, newdata = newdata, type = type)
-
     return(pred)
 }
 
 
-#' @include methods_nnet.R
+#' @include get_group_names.R
 #' @rdname get_group_names
 #' @export
-get_group_names.clm <- get_group_names.multinom
+get_group_names.clm <- function(model, ...) {
+    resp <- insight::get_response(model)
+    if (is.factor(resp)) {
+        out <- levels(resp)
+    } else {
+        out <- unique(resp)
+    }
+    return(out)
+}
--- sandbox/test-pkg-ordinal.R
+++ sandbox/test-pkg-ordinal.R
@@ -1,33 +1,56 @@
-# PROBLEM: see methods_ordinal.R for a potential bug that breaks things for us
+requiet("ordinal")
 
-skip_if_not_installed("ordinal")
-requiet("margins")
 
-stata <- readRDS(test_path("stata/stata.rds"))[["MASS_polr_01"]]
-dat <- read.csv(test_path("stata/databases/MASS_polr_01.csv"))
-mod <- ordinal::clm(factor(y) ~ x1 + x2, data = dat)
-mod_polr <- MASS::polr(factor(y) ~ x1 + x2, data = dat)
+devtools::document()
+pkgload::load_all()
 
-# Problem: all columns include the same predictions. We should have separate ones to make dy/dx for each level. This looks like a bug, so apparently we cannot support this model.
-a <- predict(mod, newdata = insight::get_data(mod), type = "prob")$fit
-b <- predict(mod, newdata = insight::get_data(mod), type = "cum.prob")[[2]]
+###############
+#  binary DV  #
+###############
 
-test_that("ordinal vs. MASS", {
-    dat <- read.csv(test_path("stata/databases/MASS_polr_01.csv"))
-    mod <- ordinal::clm(factor(y) ~ x1 + x2, data = dat)
-    mod_polr <- MASS::polr(factor(y) ~ x1 + x2, data = dat)
-    mfx <- tidy(marginaleffects(mod, type = "prob"))
-    mfx_polr <- tidy(marginaleffects(mod_polr, type = "prob"))
-})
+data(soup, package = "ordinal")
+model <- clm(factor(rating) ~ contact + temp, data = wine)
 
-test_that("ordinal: vs `margins`", {
-    data("wine", package = "ordinal")
-    tmp <- wine
-    tmp$warm <- as.numeric(tmp$temp == "warm")
-    mod <- ordinal::clm(rating ~ warm + contact, data = tmp)
-    mod2 <- MASS::polr(rating ~ warm + contact, data = tmp)
-    mfx <- marginaleffects(mod, type = "prob")
-    mar <- suppressWarnings(margins::margins(mod))
-    expect_mfx(mod, type = "prob")
-    expect_true(test_against_margins(mfx, mar, tol = .01))
-})
+get_predict(model, group_name = "1")
+
+marginaleffects(model) |> summary()
+summary(fm1)
+
+m1 <- clm(PROD ~ COLD, scale = ~prod, data = soup, link = "logit")
+m2 <- update(m1, link = "probit")
+m3 <- update(m1, link = "cloglog")
+m4 <- update(m1, link = "loglog")
+m5 <- update(m1, link = "cauchit", start = coef(m1))
+
+tidy(m1)
+tidy(marginaleffects(m3))
+
+expect_marginaleffects(m1, n_unique = 6)
+expect_marginaleffects(m2, n_unique = 6)
+expect_marginaleffects(m3, n_unique = 6)
+expect_marginaleffects(m4, n_unique = 6)
+expect_marginaleffects(m5, n_unique = 6)
+
+
+## simple model:
+####################
+#  multinomial DV  #
+####################
+
+fm1 <- clm(rating ~ contact + temp, data = wine)
+summary(fm1)
+
+## Fitted values with standard errors and confidence intervals:
+predict(fm1, se.fit=TRUE, interval=TRUE) # type="prob"
+## class predictions for the observations:
+predict(fm1, type="class")
+
+newData <- expand.grid(temp = c("cold", "warm"),
+                       contact = c("no", "yes"))
+
+## Predicted probabilities in all five response categories for each of
+## the four cases in newData:
+predict(fm1, newdata = newData, type="prob")
+
+## now include standard errors and intervals:
+predict(fm1, newdata = newData, se.fit = TRUE, interval = TRUE, type = "prob")
