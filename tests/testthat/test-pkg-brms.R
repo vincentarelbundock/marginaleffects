462571a11c80a5f8ee31c3c599e03722a18511b8
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-11-09 12:41:08 -0500

    brms predictions() + initial tests

---
+++ tests/testthat/test-pkg-brms.R
@@ -0,0 +1,35 @@
+library(brms)
+library(marginaleffects)
+library(insight)
+skip_if_not_installed("cmdstanr")
+skip_if_not_installed("brms")
+skip("brms is not officially supported yet")
+
+# pkgload::load_all()
+
+test_that("predictions: no validity", {
+  void <- capture.output(
+    mod <- brm(am ~ mpg + hp, data = mtcars, family = bernoulli(),
+               backend = "cmdstanr", seed = 1024, silent = 2, chains = 4, iter = 1000)
+  )
+  pred <- predictions(mod, newdata = typical(hp = c(100, 120)))
+  expect_equal(pred$predicted, c(0.0443223519583173, 0.119305664275307))
+  expect_equal(dim(attr(pred, "posterior_draws")), c(2, 2000))
+})
+
+test_that("plot_cap: no validity", {
+  skip("TODO: define a test")
+  void <- capture.output(
+    mod <- brm(am ~ mpg * vs, data = mtcars, family = bernoulli(),
+               backend = "cmdstanr", seed = 1024, silent = 2, chains = 4, iter = 1000)
+  )
+  p <- plot_cap(mod, condition = c("mpg", "vs"))
+})
+
+void <- capture.output(
+  mod <- brm(am ~ mpg + hp, data = mtcars, family = bernoulli(),
+              backend = "cmdstanr", seed = 1024, silent = 2, chains = 4, iter = 1000)
+)
+get_predict(mod)
+
+  predictions(mod, newdata = typical(hp = c(100, 120), mpg = seq(20, 30, by = 5)))
