aba2f04f5b42d24fd2957dfe4e9aaa4349336bc9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-12-22 09:21:56 -0500

    not sure I want to do this after all

--- R/get_predict.R
+++ R/get_predict.R
@@ -18,34 +18,60 @@ get_predict <- function(model, newdata, type, ...) {
 get_predict.default <- function(model,
                                 newdata = insight::get_data(model),
                                 type = "response",
+                                conf.level = NULL,
                                 ...) {
 
-    pred <- stats::predict(model,
-                           newdata = newdata,
-                           type = type)
+    # fast prediction for numeric differentiation
+    if (is.null(conf.level)) {
+        pred <- stats::predict(model,
+                               newdata = newdata,
+                               type = type)
 
-    # 1-d array to vector (e.g., mgcv)
-    if (is.array(pred) && length(dim(pred)) == 1) {
-        pred <- as.vector(pred)
-    }
+        # 1-d array to vector (e.g., mgcv)
+        if (is.array(pred) && length(dim(pred)) == 1) {
+            pred <- as.vector(pred)
+        }
 
-    # atomic vector
-    if (isTRUE(checkmate::check_atomic_vector(pred))) {
-        out <- data.frame(
-            rowid = 1:nrow(newdata),
-            # strip weird attributes added by some methods (e.g., predict.svyglm)
-            predicted = as.numeric(pred))
+        # atomic vector
+        if (isTRUE(checkmate::check_atomic_vector(pred))) {
+            out <- data.frame(
+                rowid = 1:nrow(newdata),
+                # strip weird attributes added by some methods (e.g., predict.svyglm)
+                predicted = as.numeric(pred))
 
-    # matrix with outcome levels as columns
-    } else if (is.matrix(pred)) {
-        out <- data.frame(
-            rowid = rep(1:nrow(pred), times = ncol(pred)),
-            group = rep(colnames(pred), each = nrow(pred)),
-            predicted = c(pred))
+        # matrix with outcome levels as columns
+        } else if (is.matrix(pred)) {
+            out <- data.frame(
+                rowid = rep(1:nrow(pred), times = ncol(pred)),
+                group = rep(colnames(pred), each = nrow(pred)),
+                predicted = c(pred))
 
+        } else {
+            stop(sprintf("Unable to extractpreditions of type %s from a model of class %s. Please report this problem, along with reproducible code and data on Github: https://github.com/vincentarelbundock/marginaleffects/issues", type, class(model)[1]))
+        }
+
+    # slow prediction with `insight::get_predicted` for `predictions`.
+    # this gives us nice back-transformed confidence intervals for many models.
     } else {
-        stop(sprintf("Unable to extractpreditions of type %s from a model of class %s. Please report this problem, along with reproducible code and data on Github: https://github.com/vincentarelbundock/marginaleffects/issues", type, class(model)[1]))
-    }
+        dots <- list(...)
+        if ("re.form" %in% names(dots)) {
+            if (is.formula(re.form)) {
+                include_random = re.form
+            } else if (is.na(re.form)) {
+                include_random = FALSE
+            } else {
+                include_random = TRUE
+            }
+        }
+        pred <- insight::get_predicted(x,
+                                       data = newdata,
+                                       predict = type,
+                                       ci = conf.level,
+                                       include_random = include_random)
+
+
+
+
 
     return(out)
 }
