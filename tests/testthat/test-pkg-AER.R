2483eb6542759d6354ac0aedcba633cc9c6486e7
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-01-01 16:43:19 -0500

    tests

--- tests/testthat/test-pkg-AER.R
+++ tests/testthat/test-pkg-AER.R
@@ -1,5 +1,7 @@
 skip_if_not_installed("AER")
 requiet("AER")
+requiet("emmeans")
+requiet("broom")
 
 test_that("marginaleffects: tobit: no validity", {
   data("Affairs", package = "AER")
@@ -19,6 +21,20 @@ test_that("marginaleffects: tobit: no validity", {
   expect_marginaleffects(mod2, n_unique = 1)
   expect_equal(mfx$estimate, mfx$dydxstata, tolerance = .1)
   expect_equal(mfx$std.error, mfx$std.errorstata, tolerance = .1)
+})
+
+test_that("marginaleffects vs. emtrends", {
+  data("Affairs", package = "AER")
+  mod <- AER::tobit(affairs ~ age + yearsmarried, data = Affairs)
+  mfx <- marginaleffects(mod, newdata = datagrid(age = 30, yearsmarried = 5))
+  em1 <- emmeans::emtrends(mod, ~age, "age", at = list(age = 30, yearsmarried = 5))
+  em2 <- emmeans::emtrends(mod, ~yearsmarried, "yearsmarried", at = list(age = 30, yearsmarried = 5))
+  em1 <- tidy(em1)
+  em2 <- tidy(em2)
+  expect_equal(mfx$dydx[1], em1$age.trend)
+  expect_equal(mfx$std.error[1], em1$std.error, tolerance = .00001)
+  expect_equal(mfx$dydx[2], em2$yearsmarried.trend)
+  expect_equal(mfx$std.error[2], em2$std.error, tolerance = .00002)
 })
 
 test_that("predictions: tobit: no validity", {
