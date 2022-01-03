8746d1bae50601be80c70b9a3df3c4b978224770
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-01-02 22:40:47 -0500

    get_predict.quantreg safety

--- R/methods_quantreg.R
+++ R/methods_quantreg.R
@@ -11,6 +11,25 @@ get_vcov.rq <- function(model, ...) {
     return(out)
 }
 
+#' @rdname get_predict
+#' @export
+get_predict.rq <- function(model,
+                           newdata = insight::get_data(model),
+                           type = NULL,
+                           conf.level = NULL,
+                           ...) {
+    assert_dependency("quantreg")
+    # type argument of the method is used to specify confidence interval type
+    # TODO: add support for this in `insight`
+    out <- quantreg::predict.rq(model,
+                                newdata = newdata,
+                                ...)
+    out <- data.frame(rowid = seq_len(nrow(newdata)),
+                      predicted = out)
+    return(out)
+}
+
+
 
 #' @include sanity_model.R
 #' @rdname sanity_model_specific
@@ -18,3 +37,5 @@ get_vcov.rq <- function(model, ...) {
 sanity_model_specific.rqs <- function(model, ...) {
     stop("`marginaleffects` only supports `quantreg::rq` models with a single `tau` value.")
 }
+
+
