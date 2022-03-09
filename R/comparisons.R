17648f2ff338bbe2377c6df03c8440c52c8c9ad0
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-03-09 00:16:15 -0500

    get_contrasts() -> comparisons()

--- R/comparisons.R
+++ R/comparisons.R
@@ -1,11 +1,34 @@
-get_contrasts <- function(model,
-                          variable,
-                          newdata = insight::get_data(model),
-                          type = "response",
-                          contrast_factor = "reference",
-                          contrast_numeric = 1,
-                          contrast_numeric_slope = FALSE,
-                          ...) {
+#' Contrasts between adjusted predictions
+#'
+#' This function calculates contrasts (or comparisons) between adjusted
+#' predictions for each row of the dataset. The resulting object can processed
+#' by the `tidy()` or `summary()` functions, which compute Average Contrasts.
+#' The `datagrid()` function and the `newdata` argument can be used to
+#' calculate contrasts Contrasts at the Mean or Contrasts at User-Specified
+#' values (aka Contrasts at Representative values). Additional information can
+#' be found in the Details and Examples sections below, and in the vignette on
+#' the `marginaleffects` website.
+#'
+#' A "contrast" is the difference between two adjusted predictions, calculated
+#' for meaningfully different regressor values (e.g., College graduates vs.
+#' Others). Uncertainty estimates are computed using the delta method.
+#'
+#' Detailed vignettes on contrasts, marginal effects, predictions, and marginal
+#' means, as well as a list of supported models can be found on the package
+#' website:
+#'
+#' https://vincentarelbundock.github.io/marginaleffects/
+#'
+#' @inheritParams marginaleffects
+#' @export
+comparisons <- function(model,
+                        variable,
+                        newdata = insight::get_data(model),
+                        type = "response",
+                        contrast_factor = "reference",
+                        contrast_numeric = 1,
+                        contrast_numeric_slope = FALSE,
+                        ...) {
 
     # if `newdata` is a call to `datagrid`, `typical`, or `counterfactual`, insert `model`
     scall <- substitute(newdata)
