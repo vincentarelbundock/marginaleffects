ff2718531f36708463bf1e2a92514b7ec2c0b2ea
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-03-24 11:52:42 -0400

    tests skip

--- tests/testthat/test-pkg-glmx.R
+++ tests/testthat/test-pkg-glmx.R
@@ -8,7 +8,6 @@ test_that("glmx: marginaleffects vs. margins", {
     model <- glmx(y ~ x, data = d, family = negative.binomial, 
                   xlink = "log", xstart = 0)
     expect_marginaleffects(model)
-
     # margins produces all zeros for se
     mar <- margins(model, unit_ses = TRUE)
     mfx <- marginaleffects(model)
@@ -16,6 +15,7 @@ test_that("glmx: marginaleffects vs. margins", {
 })
 
 test_that("predictions: glmx: no validity check", {
+    skip_if_not_installed("insight", minimum_version = "0.17.1")
     d <- data.frame(x = runif(200, -1, 1))
     d$y <- rnbinom(200, mu = exp(0 + 3 * d$x), size = 1)
     model <- glmx(y ~ x, data = d, family = negative.binomial,
@@ -25,3 +25,5 @@ test_that("predictions: glmx: no validity check", {
     expect_predictions(pred1, n_row = nrow(d))
     expect_predictions(pred2, n_row = 6)
 })
+
+
