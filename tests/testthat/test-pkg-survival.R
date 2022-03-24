4b64574f0fd6f11a369a0212a3f36a6a6365a598
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-03-24 16:07:35 -0400

    survival test fix

--- tests/testthat/test-pkg-survival.R
+++ tests/testthat/test-pkg-survival.R
@@ -4,10 +4,10 @@ requiet("broom")
 
 test_that("coxph vs. Stata", {
     stata <- readRDS(test_path("stata/stata.rds"))$survival_coxph_01
-    test1 <- list(time = c(4, 3, 1, 1, 2, 2, 3),
-                  status = c(1, 1, 1, 0, 1, 1, 0),
-                  x = c(0, 2, 1, 1, 1, 0, 0),
-                  sex = factor(c(0, 0, 0, 0, 1, 1, 1)))
+    test1 <- data.frame(time = c(4, 3, 1, 1, 2, 2, 3),
+                        status = c(1, 1, 1, 0, 1, 1, 0),
+                        x = c(0, 2, 1, 1, 1, 0, 0),
+                        sex = factor(c(0, 0, 0, 0, 1, 1, 1)))
     mod <- coxph(Surv(time, status) ~ x + strata(sex),
                  data = test1,
                  ties = "breslow")
@@ -26,20 +26,20 @@ test_that("coxph vs. Stata", {
 
 
 test_that("coxph: no validity", {
-    test2 <- list(start = c(1, 2, 5, 2, 1, 7, 3, 4, 8, 8),
-                  stop = c(2, 3, 6, 7, 8, 9, 9, 9, 14, 17),
-                  event = c(1, 1, 1, 1, 1, 1, 1, 0, 0, 0),
-                  x = c(1, 0, 0, 1, 0, 1, 1, 1, 0, 0))
+    test2 <- data.frame(start = c(1, 2, 5, 2, 1, 7, 3, 4, 8, 8),
+                        stop = c(2, 3, 6, 7, 8, 9, 9, 9, 14, 17),
+                        event = c(1, 1, 1, 1, 1, 1, 1, 0, 0, 0),
+                        x = c(1, 0, 0, 1, 0, 1, 1, 1, 0, 0))
     mod <- coxph(Surv(start, stop, event) ~ x, test2)
     expect_marginaleffects(mod, type = "risk", n_unique = 2)
 })
 
 
 test_that("bugs stay dead: conf.level forces get_predicted which doesn't process 'type'", {
-    test1 <- list(time = c(4, 3, 1, 1, 2, 2, 3),
-                  status = c(1, 1, 1, 0, 1, 1, 0),
-                  x = c(0, 2, 1, 1, 1, 0, 0),
-                  sex = factor(c(0, 0, 0, 0, 1, 1, 1)))
+    test1 <- data.frame(time = c(4, 3, 1, 1, 2, 2, 3),
+                        status = c(1, 1, 1, 0, 1, 1, 0),
+                        x = c(0, 2, 1, 1, 1, 0, 0),
+                        sex = factor(c(0, 0, 0, 0, 1, 1, 1)))
     mod <- coxph(Surv(time, status) ~ x + strata(sex),
                  data = test1,
                  ties = "breslow")
