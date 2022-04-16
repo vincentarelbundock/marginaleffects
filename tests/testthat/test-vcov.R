8c3823b01c73794eea9fdb9c1f07034dfc9e36dd
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-04-16 08:27:03 -0400

    get_varcov vcov argument now available

--- tests/testthat/test-vcov.R
+++ tests/testthat/test-vcov.R
@@ -22,3 +22,27 @@ test_that("matrix produces different results (no validity)", {
     cmp2 <- comparisons(mod, vcov = V)
     expect_true(all(cmp1$std.error != cmp2$std.error))
 })
+
+
+test_that("insight::get_varcov argument (no validity)", {
+    mod <- lm(mpg ~ hp * wt, data = mtcars)
+
+    # aliases
+    mfx1 <- marginaleffects(mod, vcov = "HC2")
+    mfx2 <- marginaleffects(mod, vcov = "stata")
+    mfx3 <- marginaleffects(mod, vcov = "HC3")
+    mfx4 <- marginaleffects(mod, vcov = "robust")
+    expect_equal(mfx1$std.error, mfx2$std.error)
+    expect_equal(mfx3$std.error, mfx4$std.error)
+
+    # different (no validity)
+    mfx5 <- marginaleffects(mod, vcov = ~ cyl)
+    mfx6 <- marginaleffects(mod, vcov = "HAC")
+    expect_true(all(mfx1$std.error != mfx3$std.error))
+    expect_true(all(mfx1$std.error != mfx4$std.error))
+    expect_true(all(mfx1$std.error != mfx5$std.error))
+    expect_true(all(mfx1$std.error != mfx6$std.error))
+    expect_true(all(mfx3$std.error != mfx5$std.error))
+    expect_true(all(mfx3$std.error != mfx6$std.error))
+    expect_true(all(mfx5$std.error != mfx6$std.error))
+})
