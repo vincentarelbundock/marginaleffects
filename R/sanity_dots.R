37a661c1d21983b796da04efa9356a636ab36bdd
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-04-26 11:55:49 -0400

    brmsfit dpar allowed

--- R/sanity_dots.R
+++ R/sanity_dots.R
@@ -5,7 +5,7 @@ sanity_dots <- function(model, ...) {
     # mixed effects
     valid[["merMod"]] <- valid[["lmerMod"]] <- valid[["glmerMod"]] <- valid[["lmerModLmerTest"]] <-
         c("include_random", "re.form", "allow.new.levels", "random.only")
-    valid[["brmsfit"]] <- c("ndraws", "re_formula", "allow_new_levels", "sample_new_levels")
+    valid[["brmsfit"]] <- c("ndraws", "re_formula", "allow_new_levels", "sample_new_levels", "dpar")
     valid[["selection"]] <- c("part") # sampleSelection
     valid[["glmmTMB"]] <- c("re.form", "allow.new.levels", "zitype") # glmmTMB
     valid[["bam"]] <- c("exclude") # mgcv
