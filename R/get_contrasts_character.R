87384339102b1ea33532088256f30e41e230aad7
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-04-29 06:40:40 -0400

    cjdt function

--- R/get_contrasts_character.R
+++ R/get_contrasts_character.R
@@ -13,13 +13,8 @@ get_contrast_data_character <- function(model,
     levs_idx$label <- sprintf("%s - %s", levs_idx$hi, levs_idx$lo)
     levs_idx <- stats::setNames(levs_idx, paste0("marginaleffects_contrast_", colnames(levs_idx)))
 
-
-    # https://github.com/Rdatatable/data.table/issues/1717#issuecomment-545758165
-    CJDT <- function(...) {
-        Reduce(function(DT1, DT2) cbind(DT1, DT2[rep(1:.N, each = nrow(DT1))]), list(...))
-    }
     setDT(newdata)
-    lo <- hi <- CJDT(newdata, levs_idx)
+    lo <- hi <- cjdt(list(newdata, levs_idx))
 
     lo[[variable]] <- lo[["marginaleffects_contrast_lo"]]
     hi[[variable]] <- hi[["marginaleffects_contrast_hi"]]
