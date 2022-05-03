3f6733b4c24d7a92b83a1a2cf7a7d81cd00c9d41
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-03 14:55:28 -0400

    clean tests

--- tests/testthat/test-pkg-brms-average.R
+++ tests/testthat/test-pkg-brms-average.R
@@ -3,22 +3,28 @@ skip_if_not_installed("cmdstanr")
 library(brms)
 library(insight)
 library(marginaleffects)
+library(magrittr)
 
 m1 <- brm(mpg ~ hp, data = mtcars, backend = "cmdstanr")
 m2 <- brm(mpg ~ hp + drat, data = mtcars, backend = "cmdstanr")
 m3 <- brm(mpg ~ hp + drat + mo(cyl), data = mtcars, backend = "cmdstanr")
 
 # pp_average() vs. predictions()
-pp_average(m1,
-           m2 = m2,
-           m3 = m3,
-           newdata = head(mtcars))
+set.seed(1024)
+pp_average(
+    m1,
+    m2 = m2,
+    m3 = m3,
+    robust = TRUE,
+    newdata = head(mtcars)) %>% suppressWarnings()
 
-predictions(m1,
-            m2 = m2,
-            m3 = m3,
-            type = "average",
-            newdata = head(mtcars))
+set.seed(1024)
+predictions(
+    m1,
+    m2 = m2,
+    m3 = m3,
+    type = "average",
+    newdata = head(mtcars)) %>% suppressWarnings()
 
 # manual vs. comparisons(): contrast of 20 about the observed values
 set.seed(1024)
@@ -30,13 +36,13 @@ avg1 <- pp_average(
     m2 = m2,
     m3 = m3,
     summary = FALSE,
-    newdata = dat_lo)
+    newdata = dat_lo) %>% suppressWarnings()
 avg2 <- pp_average(
     m1,
     m2 = m2,
     m3 = m3,
     summary = FALSE,
-    newdata = dat_hi)
+    newdata = dat_hi) %>% suppressWarnings()
 contr <- avg2 - avg1
 t(apply(contr, 2, quantile, c(.5, .025, .975)))
 
@@ -46,7 +52,7 @@ comparisons(m1,
             m3 = m3,
             type = "average",
             contrast_numeric = 20,
-            newdata = head(mtcars))
+            newdata = head(mtcars)) %>% suppressWarnings()
 
 
 # we can pass any of the arguments accepted by `pp_average`, including `method`
@@ -57,4 +63,4 @@ comparisons(m1,
             type = "average",
             method = "posterior_epred",
             contrast_numeric = 20,
-            newdata = head(mtcars))
+            newdata = head(mtcars)) %>% suppressWarnings()
