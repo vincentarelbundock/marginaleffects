44a8a8ccf25205196e603c4a0cbfaf43b8886d10
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-04 12:29:28 -0400

    mlogit: temporarily disable newdata block

--- R/methods_mlogit.R
+++ R/methods_mlogit.R
@@ -5,9 +5,15 @@ get_predict.mlogit <- function(model,
                                ...) {
 
     mat <- stats::predict(model, newdata = newdata)
+    if (isTRUE(checkmate::check_atomic_vector(mat))) {
+        out <- data.table(rowid = seq_along(mat),
+                          group = names(mat),
+                          predicted = mat)
+    } else {
     out <- data.table(rowid = rep(seq_len(nrow(mat)), rep = ncol(mat)),
                       group = rep(colnames(mat), each = nrow(mat)),
                       predicted = as.vector(mat))
+    }
     setkey(out, rowid, group)
     if ("term" %in% colnames(newdata)) {
         out[, "term" := newdata[["term"]]]
