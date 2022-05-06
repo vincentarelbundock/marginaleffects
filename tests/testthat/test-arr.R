9ad78b3aeb6ec5eb8c184a56fabe4de0746bb33d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-06 08:12:32 -0400

    transformation: safety and test error

--- tests/testthat/test-arr.R
+++ tests/testthat/test-arr.R
@@ -1,23 +1,24 @@
-# set.seed(1)
-# library(data.table)
-# library(survey)
-# setDT(nhanes)
-# setnames(nhanes, tolower(names(nhanes)))
-# nhanes[, female := riagendr == 2]
-# nhanes[, race := sprintf("race%s", race)]
-# nhanes[, rnd := rnorm(.N, 10, 5)]
-# mod <- glm(hi_chol ~ female,
-#            data = nhanes,
-#            family = binomial)
+requiet("survey")
+data(nhanes, package = "survey")
+dat <- setNames(nhanes, tolower(names(nhanes)))
+dat$female <- dat$riagendr == 2
+dat$race <- sprintf("race%s", dat$race)
+mod <- glm(hi_chol ~ female, data = dat, family = binomial)
 # pkgload::load_all()
-#
+
+test_that("error when function breaks or returns a bad vector", {
+    expect_error(comparisons(mod, transformation = mean),
+                 regexp = "numeric vector")
+    expect_error(comparisons(mod, transformation = function(hi, lo) head(hi - lo)),
+                 regexp = "numeric vector")
+})
+
 # # default
 # comparisons(mod) |> summary()
 #
 # # default: specified manually
 # comparisons(mod, transformation = \(hi, lo) hi - lo) |> summary()
 #
-# #  
 # comparisons(mod, transformation = \(hi, lo) hi / lo) |> summary()
 #
 # comparisons(mod, transformation = \(hi, lo) log(hi) - log(lo)) |> summary()
