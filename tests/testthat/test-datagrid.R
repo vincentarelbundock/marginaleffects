d28ca28f9c4cacb76aad486f9dabc55a24ff85e1
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-07 11:14:26 -0400

    datagrid improvements?

---
+++ tests/testthat/test-datagrid.R
@@ -0,0 +1,11 @@
+
+test_that("numeric clusters warning", {
+    requiet("lme4")
+    requiet("fixest")
+
+    mod <- lmer(mpg ~ hp + (1 + drat | cyl), data = mtcars)
+    expect_warning(datagrid(model = mod), regex = "cluster")
+
+    mod <- feols(mpg ~ hp | cyl, data = mtcars)
+    expect_warning(datagrid(model = mod), regex = "cluster")
+})
