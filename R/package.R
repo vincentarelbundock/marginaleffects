14d4ed12039f79a6fb2c114c7f5ae7bc818fbdca
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-09 10:31:32 -0400

    checks

--- R/package.R
+++ R/package.R
@@ -1,13 +1,12 @@
 # https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when#comment20826625_12429344
 # 2012 hadley says "globalVariables is a hideous hack and I will never use it"
-# 2014 hadley updates his own answer with globalVariables as one of "two solutions"
+# 2014 hadley updates his own answer with globalVariables as one of "two
+# solutions"
 #' @import data.table
-utils::globalVariables(c('.', 'term', 'part', 'estimate', 'conf.high',
-                         'conf.low', 'value', 'p.value', 'std.error',
-                         'statistic', 'stars_note', 'logLik', 'formatBicLL',
-                         'section', 'position', 'where', 'ticks', 'statistic1',
-                         'model', 'tmp_grp', 'condition_variable', 'conf_int',
-                         'conf_level', 'group', 'contrast', 'm', 'dydx',
-                         "patterns", 'rowid', 'predicted', 'gn',
-                         # data.table calls
-                         'comparison', "..idx", "..idx_by", "..idx_lo", "..idx_hi", "..tmp", "draw", "..cols"))
+utils::globalVariables( c('.', 'term', 'part', 'estimate', 'conf.high',
+    'conf.low', 'value', 'p.value', 'std.error', 'statistic', 'stars_note',
+    'logLik', 'formatBicLL', 'section', 'position', 'where', 'ticks',
+    'statistic1', 'model', 'tmp_grp', 'condition_variable', 'conf_int',
+    'conf_level', 'group', 'contrast', 'm', 'dydx', "patterns", 'rowid',
+    'predicted', 'gn',  "predicted_lo", "predicted_hi", 'comparison', "..idx",
+    "..idx_by", "..idx_lo", "..idx_hi", "..tmp", "draw", "..cols"))
