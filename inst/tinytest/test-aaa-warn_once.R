f2f64e491fccbb5770b3f858c7a483e4de5a1afc
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-17 08:18:07 -0400

    intentional breakage

--- inst/tinytest/test-aaa-warn_once.R
+++ inst/tinytest/test-aaa-warn_once.R
@@ -1,5 +1,8 @@
 source("helpers.R")
 
+# TODO: intentional breakage to test github actions
+tinytest::expect_true(FALSE)
+
 # factor in formula
 mod <- lm(mpg ~ hp + factor(cyl), data = mtcars)
 expect_warning(marginaleffects(mod))
