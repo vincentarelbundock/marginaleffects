8e008266785eb576311e5740aace93a7a8c62d4b
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-17 13:32:24 -0400

    snapshots

--- inst/tinytest/test-comparisons.R
+++ inst/tinytest/test-comparisons.R
@@ -4,6 +4,12 @@ source("helpers.R")
 dat <- mtcars
 dat$gear <- factor(dat$gear)
 mod <- lm(mpg ~ gear, data = dat)
-# expect_snapshot(summary(comparisons(mod)))
-#
+cmp <- comparisons(mod)
+known <- "Average contrasts
+  Term Contrast Effect Std. Error z value   Pr(>|z|)  2.5 % 97.5 %
+1 gear    4 - 3  8.427      1.823   4.621 3.8123e-06 4.8528  12.00
+2 gear    5 - 3  5.273      2.431   2.169   0.030082 0.5082  10.04
 
+Model type:  lm
+Prediction type:  response"
+expect_print(summary(cmp), known)
