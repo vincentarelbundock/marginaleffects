ab46cc6b860f34c9f726a0d946735ead207a5b4c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-17 07:17:43 -0400

    skip tests on cran

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,6 @@
-if (requireNamespace("tinytest", quietly = TRUE)) {
-    tinytest::test_package("marginaleffects")
+# skip all tests on CRAN
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+    if (requireNamespace("tinytest", quietly = TRUE)) {
+        tinytest::test_package("marginaleffects")
+    }
 }
