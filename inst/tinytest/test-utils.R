ee858470e80e7803a6cc8825cce5ff1c18bb5bce
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-05-22 16:19:53 -0400

    docs and tests

--- inst/tinytest/test-utils.R
+++ inst/tinytest/test-utils.R
@@ -9,11 +9,11 @@ expect_error(marginaleffects:::find_variable_class(
 
 
 
-# mean_or_mode
+# marginaleffects::mean_or_mode
 x <- factor(c("a", "a", "b"))
-expect_equivalent(mean_or_mode(x), x[1])
-expect_equivalent(mean_or_mode(c("a", "a", "b")), "a")
-expect_equivalent(mean_or_mode(1:3), 2)
-expect_equivalent(mean_or_mode(c(FALSE, FALSE, TRUE)), FALSE)
-expect_equivalent(mean_or_mode(data.frame(x = 1:3))$x, 2)
+expect_equivalent(marginaleffects::mean_or_mode(x), x[1])
+expect_equivalent(marginaleffects::mean_or_mode(c("a", "a", "b")), "a")
+expect_equivalent(marginaleffects::mean_or_mode(1:3), 2)
+expect_equivalent(marginaleffects::mean_or_mode(c(FALSE, FALSE, TRUE)), FALSE)
+expect_equivalent(marginaleffects::mean_or_mode(data.frame(x = 1:3))$x, 2)
 
