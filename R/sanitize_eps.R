4e2fda2f409ff2e41832489616f71d6320c7e2ea
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-06-08 11:18:27 -0400

    code comment

--- R/sanitize_eps.R
+++ R/sanitize_eps.R
@@ -2,6 +2,7 @@ sanitize_eps <- function(eps, model, variables) {
     checkmate::assert_number(eps, null.ok = TRUE)
     if (is.null(eps)) {
         out <- list(default_eps = 1e-4)
+        # we don't use `newdata`, because that may already be averaged or subsetted
         dat <- insight::get_data(model)
         for (v in unlist(variables)) {
             if (isTRUE(is.numeric(dat[[v]]))) {
