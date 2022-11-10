116a8facafdf38392d4654711a76bc5ba8c0953d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-11-10 17:59:43 -0500

    workflow test

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,6 @@
+print(Sys.getenv("R_NOT_CRAN"))
+cat(Sys.getenv("R_NOT_CRAN"))
+
 if (requireNamespace("tinytest", quietly = TRUE) && isTRUE(Sys.getenv("R_NOT_CRAN") == "true")) {
     tinytest::test_package("marginaleffects")
 }
