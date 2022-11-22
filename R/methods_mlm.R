4d2bd2a4f02941f88d752b1a11682a9c3f30f2e9
Noah Greifer <noah.greifer@gmail.com>
2022-11-22 17:13:54 -0500

    Added support for mlm (multivariate lm) models. Didn't write any tests.

---
+++ R/methods_mlm.R
@@ -0,0 +1,31 @@
+#' @include set_coef.R
+#' @rdname set_coef
+#' @export
+set_coef.mlm <- function(model, coefs, ...) {
+
+  model$coefficients[] <- coefs
+
+  return(model)
+}
+
+#' @include get_coef.R
+#' @rdname get_coef
+#' @export
+get_coef.mlm <- function(model, ...) {
+  out <- insight::get_parameters(model, ...)
+  out <- stats::setNames(
+    out$Estimate,
+    sprintf("%s:%s", out$Response, out$Parameter))
+  return(out)
+}
+
+
+#' @include get_group_names.R
+#' @rdname get_group_names
+#' @export
+get_group_names.mlm <- function(model, ...) {
+  resp <- insight::get_response(model)
+  return(names(resp))
+}
+
+
