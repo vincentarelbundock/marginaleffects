0c5e8adbbb36c7aa091ff8f8867c2e969c8b3ed4
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-21 17:18:44 -0500

    bug arises with refactored insight::get_data

--- inst/tinytest/test-plot_cco.R
+++ inst/tinytest/test-plot_cco.R
@@ -3,6 +3,9 @@ source("helpers.R")
 # not supported because datplot is not representative, so we can't take minmax or iqr
 mod <- lm(mpg ~ wt * hp, data = mtcars)
 
+
+
+
 p <- plot_cco(mod, effect = list(hp = "minmax"), condition = "wt", draw = FALSE)
 expect_equivalent(length(unique(p$comparison)), 25)
 p <- plot_cco(mod, effect = list(hp = "minmax"), condition = "wt")
@@ -14,3 +17,9 @@ expect_inherits(p, "gg")
 
 # one effect at a time
 expect_error(plot_cco(mod, effect = c("hp", "wt"), condition = "wt"), pattern = "length")
+
+
+# bug from examples (revise_get_data() no longer returns a factor attribute)
+mod <- lm(mpg ~ hp * drat * factor(am), data = mtcars)
+p <- plot_cco(mod, effect = "hp", condition = list("am", "drat" = 3:5), draw = FALSE)
+expect_inherits(p, "data.frame")
