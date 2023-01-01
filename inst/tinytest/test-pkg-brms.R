98e7db32d026cca24261bf10244dec469493981f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-01-01 09:25:51 -0500

    reinstante brms tests

--- inst/tinytest/test-pkg-brms.R
+++ inst/tinytest/test-pkg-brms.R
@@ -3,7 +3,7 @@
 # https://github.com/vincentarelbundock/marginaleffects/issues/240
 source("helpers.R", local = TRUE)
 using("marginaleffects")
-exit_file("expensive")
+# exit_file("expensive")
 # if (ON_CI) exit_file("on ci")
 if (ON_WINDOWS) exit_file("on windows")
 if (ON_CRAN) exit_file("on cran")
