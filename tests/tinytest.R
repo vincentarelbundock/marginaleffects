0e2617e9a6ad7e49ee678ef2f020eca180c4d29e
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-01-04 14:43:12 -0500

    tests

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,5 @@
-if (requireNamespace("tinytest", quietly = TRUE) && isTRUE(Sys.getenv("R_NOT_CRAN") == "true")) {
+if (requireNamespace("tinytest", quietly = TRUE) &&
+    isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
+    isTRUE(Sys.info()["sysname"] != "Windows")) {
     tinytest::test_package("marginaleffects")
 }
