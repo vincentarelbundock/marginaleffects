3dc27a124209b2f68b0dd9d10be9c3870f6a00e1
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-01-08 19:12:48 -0500

    simplify print

--- R/print.R
+++ R/print.R
@@ -16,9 +16,6 @@ print.slopes.summary <- function(x,
         out$group <- NULL
     }
 
-    # title
-    tit <- "Average marginal effects"
-
     # round and replace NAs
     for (col in c("estimate", "std.error", "statistic", "conf.low", "conf.high")) {
         if (col %in% colnames(out)) {
@@ -28,7 +25,6 @@ print.slopes.summary <- function(x,
     if ("p.value" %in% colnames(out)) {
         out[["p.value"]] <- format.pval(out[["p.value"]])
     }
-
 
     if (is.null(attr(x, "conf_level"))) {
         alpha <- NULL
@@ -50,6 +46,7 @@ print.slopes.summary <- function(x,
         "group" = "Group",
         "term" = "Term",
         "contrast" = "Contrast",
+        "value" = "Value",
         "estimate" = "Effect",
         "std.error" = "Std. Error",
         "statistic" = "z",
@@ -61,16 +58,23 @@ print.slopes.summary <- function(x,
             "CI high",
             sprintf("%.1f %%", 100 - alpha / 2)))
 
+    if (inherits(x, "marginalmeans.summary")) {
+        dict["estimate"] <- "Mean"
+    }
+
+    if (all(out$term == "cross")) {
+        out[["term"]] <- NULL
+        colnames(out) <- gsub("^contrast_", "", colnames(out))
+    }
+
     for (i in seq_along(dict)) {
         colnames(out)[colnames(out) == names(dict)[i]] <- dict[i]
     }
-
 
     # avoid infinite recursion by stripping marginaleffect.summary class
     out <- as.data.frame(out)
 
     # some commands do not generate average contrasts/mfx. E.g., `lnro` with `by`
-    # cat(tit)
     print(out)
     cat("\n")
     cat("Model type: ", attr(x, "model_type"), "\n")
@@ -81,10 +85,11 @@ print.slopes.summary <- function(x,
     if (!is.null(attr(x, "transform_post_label"))) {
         cat("Post-transformation: ", paste(attr(x, "transform_post_label"), collapse = ""), "\n")
     }
-    if (!is.null(attr(x, "transform_average_label"))) {
-        cat("Average-transformation: ", paste(attr(x, "transform_average_label"), collapse = ""), "\n")
+    vg <- attr(x, "variables_grid")
+    if (length(vg) > 0) {
+        cat(sprintf("Results averaged over levels of: %s",
+                    paste(vg, collapse = ", ")), "\n")
     }
-
 
     return(invisible(x))
 }
@@ -92,244 +97,14 @@ print.slopes.summary <- function(x,
 
 #' @noRd
 #' @export
-print.predictions.summary <- function(x,
-                                      digits = max(3L, getOption("digits") - 3L),
-                                      ...) {
-
-  out <- x
-
-  # title
-  tit <- "Average Adjusted Predictions"
-
-  # round and replace NAs
-  for (col in c("estimate", "std.error", "statistic", "conf.low", "conf.high")) {
-    if (col %in% colnames(out)) {
-      out[[col]] <- format(out[[col]], digits = digits)
-    }
-  }
-  if ("p.value" %in% colnames(out)) {
-      out[["p.value"]] <- format.pval(out[["p.value"]])
-  }
-
-
-  if (is.null(attr(x, "conf_level"))) {
-      alpha <- NULL
-  } else {
-      alpha <- 100 * (1 - attr(x, "conf_level"))
-  }
-
-  # contrast is sometimes useless
-  if ("contrast" %in% colnames(out) && all(out$contrast == "")) {
-      out$contrast <- NULL
-  }
-
-  if ("type" %in% colnames(out) && length(unique(out$type)) == 1) {
-      out$type <- NULL
-  }
-
-  # rename
-  dict <- c("group" = "Group",
-            "term" = "Term",
-            "estimate" = "Predicted",
-            "std.error" = "Std. Error",
-            "statistic" = "z",
-            "p.value" = "Pr(>|z|)",
-            "conf.low" = ifelse(is.null(alpha),
-                                "CI low",
-                                sprintf("%.1f %%", alpha / 2)),
-            "conf.high" = ifelse(is.null(alpha),
-                                "CI high",
-                                sprintf("%.1f %%", 100 - alpha / 2)))
-
-  for (i in seq_along(dict)) {
-    colnames(out)[colnames(out) == names(dict)[i]] <- dict[i]
-  }
-
-
-  # avoid infinite recursion by stripping marginaleffect.summary class
-  out <- as.data.frame(out)
-
-  # some commands do not generate average contrasts/mfx. E.g., `lnro` with `by`
-  # cat(tit, "\n")
-  print(out)
-  cat("\n")
-  cat("Model type: ", attr(x, "model_type"), "\n")
-  cat("Prediction type: ", attr(x, "type"), "\n")
-  if (!is.null(attr(x, "transform_pre_label"))) {
-      cat("Pre-transformation: ", paste(attr(x, "transform_pre_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_post_label"))) {
-      cat("Post-transformation: ", paste(attr(x, "transform_post_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_average_label"))) {
-      cat("Average-transformation: ", paste(attr(x, "transform_average_label"), collapse = ""), "\n")
-  }
-
-
-  return(invisible(x))
-}
+print.predictions.summary <- print.slopes.summary
 
 
 #' @noRd
 #' @export
-print.marginalmeans.summary <- function(x,
-                                        digits = max(3L, getOption("digits") - 3L),
-                                        ...) {
-
-  out <- x
-
-  # title
-  tit <- "Estimated marginal means"
-
-  # round and replace NAs
-  for (col in c("estimate", "std.error", "statistic", "conf.low", "conf.high")) {
-    if (col %in% colnames(out)) {
-      out[[col]] <- format(out[[col]], digits = digits)
-    }
-  }
-  if ("p.value" %in% colnames(out)) {
-      out[["p.value"]] <- format.pval(out[["p.value"]])
-  }
-
-
-  if (is.null(attr(x, "conf_level"))) {
-      alpha <- NULL
-  } else {
-      alpha <- 100 * (1 - attr(x, "conf_level"))
-  }
-
-
-  # rename
-  dict <- c("group" = "Group",
-            "term" = "Term",
-            "contrast" = "Contrast",
-            "value" = "Value",
-            "estimate" = "Mean",
-            "std.error" = "Std. Error",
-            "statistic" = "z",
-            "p.value" = "Pr(>|z|)",
-            "conf.low" = ifelse(is.null(alpha),
-                                "CI low",
-                                sprintf("%.1f %%", alpha / 2)),
-            "conf.high" = ifelse(is.null(alpha),
-                                "CI high",
-                                sprintf("%.1f %%", 100 - alpha / 2)))
-
-  for (i in seq_along(dict)) {
-    colnames(out)[colnames(out) == names(dict)[i]] <- dict[i]
-  }
-
-  # avoid infinite recursion by stripping marginaleffect.summary class
-  out <- as.data.frame(out)
-
-  print(out)
-  cat("\n")
-  cat("Model type: ", attr(x, "model_type"), "\n")
-  cat("Prediction type: ", attr(x, "type"), "\n")
-
-  vg <- attr(x, "variables_grid")
-  if (length(vg) > 0) {
-    cat(sprintf("Results averaged over levels of: %s",
-                paste(vg, collapse = ", ")), "\n")
-  }
-  if (!is.null(attr(x, "transform_pre_label"))) {
-      cat("Pre-transformation: ", paste(attr(x, "transform_pre_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_post_label"))) {
-      cat("Post-transformation: ", paste(attr(x, "transform_post_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_average_label"))) {
-      cat("Average-transformation: ", paste(attr(x, "transform_average_label"), collapse = ""), "\n")
-  }
-
-
-  return(invisible(x))
-}
+print.marginalmeans.summary <- print.slopes.summary
 
 
 #' @noRd
 #' @export
-print.comparisons.summary <- function(x,
-                                      digits = max(3L, getOption("digits") - 3L),
-                                      ...) {
-
-  out <- x
-
-
-  # title
-  tit <- "Average contrasts"
-
-  # round and replace NAs
-  for (col in c("estimate", "std.error", "statistic", "conf.low", "conf.high")) {
-    if (col %in% colnames(out)) {
-      out[[col]] <- format(out[[col]], digits = digits)
-    }
-  }
-  if ("p.value" %in% colnames(out)) {
-      out[["p.value"]] <- format.pval(out[["p.value"]])
-  }
-
-
-  if (is.null(attr(x, "conf_level"))) {
-      alpha <- NULL
-  } else {
-      alpha <- 100 * (1 - attr(x, "conf_level"))
-  }
-
-  # contrast is sometimes useless
-  if ("contrast" %in% colnames(out) && all(out$contrast == "")) {
-      out$contrast <- NULL
-  }
-
-  if ("type" %in% colnames(out) && length(unique(out$type)) == 1) {
-      out$type <- NULL
-  }
-
-  # rename
-  dict <- c("group" = "Group",
-            "term" = "Term",
-            "contrast" = "Contrast",
-            "estimate" = "Effect",
-            "std.error" = "Std. Error",
-            "statistic" = "z",
-            "p.value" = "Pr(>|z|)",
-            "conf.low" = ifelse(is.null(alpha),
-                                "CI low",
-                                sprintf("%.1f %%", alpha / 2)),
-            "conf.high" = ifelse(is.null(alpha),
-                                "CI high",
-                                sprintf("%.1f %%", 100 - alpha / 2)))
-
-  if (all(out$term == "interaction")) {
-    out[["term"]] <- NULL
-    colnames(out) <- gsub("^contrast_", "", colnames(out))
-  }
-
-  for (i in seq_along(dict)) {
-    colnames(out)[colnames(out) == names(dict)[i]] <- dict[i]
-  }
-
-
-  # avoid infinite recursion by stripping marginaleffect.summary class
-  out <- as.data.frame(out)
-
-  # some commands do not generate average contrasts/mfx. E.g., `lnro` with `by`
-  # cat(tit, "\n")
-  print(out)
-  cat("\n")
-  cat("Model type: ", attr(x, "model_type"), "\n")
-  cat("Prediction type: ", attr(x, "type"), "\n")
-  if (!is.null(attr(x, "transform_pre_label"))) {
-      cat("Pre-transformation: ", paste(attr(x, "transform_pre_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_post_label"))) {
-      cat("Post-transformation: ", paste(attr(x, "transform_post_label"), collapse = ""), "\n")
-  }
-  if (!is.null(attr(x, "transform_average_label"))) {
-      cat("Average-transformation: ", paste(attr(x, "transform_average_label"), collapse = ""), "\n")
-  }
-
-
-  return(invisible(x))
-}
-
+print.comparisons.summary <- print.slopes.summary
