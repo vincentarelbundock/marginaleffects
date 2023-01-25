60fb4b1cc4c0f584c0aac79fdd2819a10586fe87
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-01-25 16:42:08 -0500

    docs

--- man-roxygen/equivalence.R
+++ man-roxygen/equivalence.R
@@ -1,22 +1,25 @@
 #' @section Equivalence, Inferiority, Superiority:
 #'
-#' \eqn{\theta} is an estimate, \eqn{\sigma_\theta} its estimated standard error, and \eqn{[a, b]} are the bounds of the equivalence region supplied to the `equivalence` argument.
+#' \eqn{\theta} is an estimate, \eqn{\sigma_\theta} its estimated standard error, and \eqn{[a, b]} are the bounds of the interval supplied to the `equivalence` argument.
 #' 
-#' The test statistics are:
+#' Non-inferiority:
 #' 
-#' \itemize{
-#' \item Non-inferiority: \eqn{t=(\theta - a)/\sigma_\theta}{t=(\theta - a)/\sigma_\theta}
-#' \item Non-superiority: \eqn{t=(\theta - b)/\sigma_\theta}{t=(\theta - b)/\sigma_\theta}
-#' }
+#' - \eqn{H_0}{H0}: \eqn{\theta \leq a}{\theta <= a}
+#' - \eqn{H_1}{H1}: \eqn{\theta > a}
+#' - \eqn{t=(\theta - a)/\sigma_\theta}{t=(\theta - a)/\sigma_\theta}
+#' - p: Upper-tail probability
 #' 
-#' The p values are:
+#' Non-superiority:
 #' 
-#' \itemize{
-#' \item Non-inferiority: Upper tail probability
-#' \item Non-superiority: Lower tail probability
-#' \item Equivalence (TOST): Maximum of the non-inferiority and non-superiority p values.
-#' }
+#' - \eqn{H_0}{H0}: \eqn{\theta \geq b}{\theta >= b}
+#' - \eqn{H_1}{H1}: \eqn{\theta < b}
+#' - \eqn{t=(\theta - b)/\sigma_\theta}{t=(\theta - b)/\sigma_\theta}
+#' - p: Lower-tail probability
+#' 
+#' Equivalence (TOST):
+#' 
+#' - \eqn{H_0}{H0}: \eqn{\theta > b} and \eqn{\theta < a}
+#' - p: Maximum of the non-inferiority and non-superiority p values.
 #' 
 #' Thanks to Russell V. Lenth for the excellent `emmeans` package and documentation which inspired this feature.
 #' 
- 
