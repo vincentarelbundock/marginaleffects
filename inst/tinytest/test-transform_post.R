67ff516dace09819df1ed7ec12fc4aeb4ed00eb6
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-02-09 10:15:40 -0500

    drop test

--- inst/tinytest/test-transform_post.R
+++ inst/tinytest/test-transform_post.R
@@ -30,10 +30,3 @@ expect_equivalent(exp(cmp1$conf.high), cmp2$conf.high)
 # expect_equivalent(exp(tid1$estimate), tid2$estimate)
 # expect_equivalent(exp(tid1$conf.low), tid2$conf.low)
 # expect_equivalent(exp(tid1$conf.high), tid2$conf.high)
-
-# string shortcuts and printout
-mod <- lm(mpg ~ hp, mtcars)
-cmp <- comparisons(mod, transform_post = "exp")
-expect_inherits(cmp, "comparisons")
-pri <- capture.output(summary(cmp))
-expect_equivalent(sum(grepl(" exp ", pri)), 1)
