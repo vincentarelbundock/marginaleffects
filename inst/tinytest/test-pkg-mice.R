ef3428adac3b8f8185b5154a51d3841ab0ed217a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-02-12 16:46:11 -0500

    failing test intential needs fix

--- inst/tinytest/test-pkg-mice.R
+++ inst/tinytest/test-pkg-mice.R
@@ -8,6 +8,7 @@ dat$Species[sample(seq_len(nrow(iris)), 40)] <- NA
 dat_mice <- mice::mice(dat, m = 20, printFlag = FALSE, .Random.seed = 1024)
 mod <- lm(Petal.Width ~ Sepal.Length * Sepal.Width + Species, data = dat)
 
-mfx <- suppressWarnings(inferences(avg_slopes(mod, by = "Species"), method = "mi", midata = dat_mice))
-expect_inherits(mfx, "comparisons")
-expect_equivalent(nrow(mfx), 4)
+mfx1 <- suppressWarnings(inferences(avg_slopes(mod, by = "Species"), method = "mi", midata = dat_mice))
+mfx2 <- suppressWarnings(inferences(avg_slopes(mod, by = "Species"), method = "mi", midata = dat_mice))
+expect_inherits(mfx1, "comparisons")
+expect_equivalent(nrow(mfx1), nrow(mfx2))
