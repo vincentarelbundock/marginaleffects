adc6dbee7a3abb18e10ec33d68c6ac7f9941d937
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-02-12 22:56:21 -0500

    comment

--- inst/tinytest/test-misc.R
+++ inst/tinytest/test-misc.R
@@ -1,9 +1,9 @@
 source("helpers.R")
 
+# important for modelsummary glance
 tmp <- mtcars
 tmp$am <- as.logical(tmp$am)
 mod <- lm(mpg ~ am + factor(cyl), tmp)
-
 expect_inherits(attr(predictions(mod), "model"), "lm")
 expect_inherits(attr(comparisons(mod), "model"), "lm")
 expect_inherits(attr(avg_slopes(mod), "model"), "lm")
