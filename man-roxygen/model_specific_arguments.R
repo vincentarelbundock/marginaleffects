94f70e69a8dc40960bb47a13b0b2a63a93caacb5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-02-26 08:34:58 -0500

    docs

--- man-roxygen/model_specific_arguments.R
+++ man-roxygen/model_specific_arguments.R
@@ -6,9 +6,8 @@
 #' | Package     | Class      | Argument           | Documentation |
 #' |-------------|------------|--------------------|---------------|
 #' | `brms`      | `brmsfit`  | `ndraws`           |[brms::posterior_predict]|
-#' |             |            | `re_formula`       ||
-#' | `lme4`      | `merMod`   | `include_random`   |[insight::get_predicted]|
-#' |             |            | `re.form`          |[lme4::predict.merMod]|
+#' |             |            | `re_formula`       |[brms::posterior_predict]|
+#' | `lme4`      | `merMod`   | `re.form`          |[lme4::predict.merMod]|
 #' |             |            | `allow.new.levels` |[lme4::predict.merMod]|
 #' | `glmmTMB`   | `glmmTMB`  | `re.form`          |[glmmTMB::predict.glmmTMB]|
 #' |             |            | `allow.new.levels` |[glmmTMB::predict.glmmTMB]|
