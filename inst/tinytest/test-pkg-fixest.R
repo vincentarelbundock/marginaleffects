2523143a59bba86974cecfbd662ab49199fbf2c9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-03-03 09:16:11 -0500

    fix tests

--- inst/tinytest/test-pkg-fixest.R
+++ inst/tinytest/test-pkg-fixest.R
@@ -257,16 +257,13 @@ expect_false(anyNA(p$std.error))
 
 
 # Issue #705
-library(fixest)
-
 data(trade, package = "fixest")
-data(mpdta, package = "etwfe")
 mod1 <- fepois(data = trade, Euros ~ 1 | Origin, offset = ~ log(dist_km))
 mod2 <- fepois(data = trade, Euros ~ 1 | Origin)
 
 mfx1 <- avg_slopes(mod1)
 mfx2 <- avg_slopes(mod2)
-xpect_inherits(mfx1, "slopes")
+expect_inherits(mfx1, "slopes")
 expect_inherits(mfx2, "slopes")
 
 
