33d71c697eda0ee1cae35f96ca067b4b68e62833
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-03-06 08:45:24 -0500

    memory optimization

--- R/get_se_delta.R
+++ R/get_se_delta.R
@@ -114,7 +114,7 @@ get_se_delta <- function(model,
     # Var(dydx) = J Var(beta) J'
     # computing the full matrix is memory-expensive, and we only need the diagonal
     # algebra trick: https://stackoverflow.com/a/42569902/342331
-    se <- sqrt(colSums(t(J %*% V) * t(J)))
+    se <- sqrt(rowSums(tcrossprod(J, V) * J))
     se[se == 0] <- NA_real_
     attr(se, "jacobian") <- J
 
