4f9a4c49fb6e2f5fa8e29ea115fc6935f65788b1
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-03-30 10:05:10 -0400

    test-pkg-phylolm

--- inst/tinytest/test-pkg-phylolm.R
+++ inst/tinytest/test-pkg-phylolm.R
@@ -1,4 +1,6 @@
-library(phylolm)
+source("helpers.R")
+requiet("phylolm")
+
 set.seed(123456)
 tre = rcoal(60)
 taxa = sort(tre$tip.label)
@@ -10,7 +12,7 @@ x <- rTrait(
 y <- b0 + b1 * x +
     rTrait(n = 1, phy = tre, model = "lambda", parameters = list(
         ancestral.state = 0, sigma2 = 1, lambda = 0.5))
-dat <- data.frame(trait = y[taxa], pred = x[taxa])
+dat <<- data.frame(trait = y[taxa], pred = x[taxa])
 fit <- phylolm(trait ~ pred, data = dat, phy = tre, model = "lambda")
 pre <- predictions(fit)
 cmp <- comparisons(fit)
@@ -18,3 +20,7 @@ mfx <- avg_slopes(fit)
 expect_inherits(pre, "predictions")
 expect_inherits(cmp, "comparisons")
 expect_inherits(mfx, "slopes")
+
+
+source("helpers.R")
+rm(list = ls())
