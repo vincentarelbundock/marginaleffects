3514ef94e29ac2885ae597fd7dfb8426c9c6bd23
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-06-12 11:37:53 -0400

    test

--- inst/tinytest/test-pkg-bife.R
+++ inst/tinytest/test-pkg-bife.R
@@ -18,5 +18,29 @@ pred <- predictions(mod, newdata = dat)
 expect_predictions(pred, n_row = nrow(dat))
 
 
+# Issue 809
+data(psid, package = "bife")
+
+psid <- transform(psid, KID = ifelse(KID1 > 0, 1, 0))
+mod <- bife(LFP ~ KID + AGE + KID * AGE + log(INCH) | ID, data = psid, model = "probit")
+
+s1 <- avg_slopes(mod, variables = "AGE", by = "KID", newdata = psid)
+xb <- predict(mod, type = "link", X_new = psid)
+psid$s <- dnorm(xb) * (coef(mod)["AGE"] + coef(mod)["KID:AGE"] * psid$KID)
+s2 <- aggregate(s ~ KID, FUN = mean, data = psid)
+s2 <- s2[order(s2$KID),]
+s1 <- s1[order(s1$KID),]
+expect_equivalent(s1$estimate, s2$s, tolerance = 1e-4)
+
+mod <- bife::bias_corr(mod)
+s1 <- avg_slopes(mod, variables = "AGE", by = "KID", newdata = psid)
+xb <- predict(mod, type = "link", X_new = psid)
+psid$s <- dnorm(xb) * (coef(mod)["AGE"] + coef(mod)["KID:AGE"] * psid$KID)
+s2 <- aggregate(s ~ KID, FUN = mean, data = psid)
+s2 <- s2[order(s2$KID),]
+s1 <- s1[order(s1$KID),]
+expect_equivalent(s1$estimate, s2$s, tolerance = 1e-4)
+
+
 
 rm(list = ls())
