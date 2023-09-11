b2f519ef5d221c601a69f133aef2928ad633e6ea
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-09-11 07:53:28 -0400

    minor

--- inst/tinytest/test-pkg-robustlmm.R
+++ inst/tinytest/test-pkg-robustlmm.R
@@ -5,9 +5,10 @@ using("marginaleffects")
 requiet("robustlmm")
 requiet("emmeans")
 requiet("broom")
+requiet("lme4")
 
 # no validity
-mod <- rlmer(Reaction ~ Days + (Days | Subject), sleepstudy,
+mod <- robustlmm::rlmer(Reaction ~ Days + (Days | Subject), sleepstudy,
     rho.sigma.e = psi2propII(smoothPsi, k = 2.28),
     rho.sigma.b = chgDefaults(smoothPsi, k = 5.11, s = 10))
 expect_predictions(predictions(mod))
