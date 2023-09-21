69180dd2cdc9bf5c481b8701006121f11ce5ca1e
Tristan-Siegfried <n>
2023-09-21 14:32:19 -0500

    Change remaining `FUN` args in `hypotheses`

--- inst/tinytest/test-hypotheses.R
+++ inst/tinytest/test-hypotheses.R
@@ -5,7 +5,7 @@ requiet("car")
 
 
 
-# When `FUN` and `hypotheses` are `NULL`, `hypotheses()` returns a data.frame of parameters
+# When `hypoth_fun` and `hypotheses` are `NULL`, `hypotheses()` returns a data.frame of parameters
 dat <- mtcars
 mod <- lm(mpg ~ hp + wt + factor(cyl), data = dat)
 dmm <- hypotheses(mod)
@@ -25,7 +25,7 @@ expect_inherits(dmm, "data.frame")
 dmm <- hypotheses(mod, "hp = wt", vcov = "HC3")
 expect_inherits(dmm, "data.frame")
 
-# b1, b2, ... shortcuts can be used to identify rows in the output of FUN
+# b1, b2, ... shortcuts can be used to identify rows in the output of hypoth_fun
 dmm <- hypotheses(mod, "b2 = b3")
 expect_inherits(dmm, "data.frame")
 
@@ -33,24 +33,24 @@ expect_inherits(dmm, "data.frame")
 dmm <- hypotheses(mod, "`factor(cyl)6` = `factor(cyl)8`")
 expect_inherits(dmm, "data.frame")
 
-# The `FUN` argument can be used to compute standard errors for fitted values
+# The `hypoth_fun` argument can be used to compute standard errors for fitted values
 mod <- glm(am ~ hp + mpg, data = mtcars, family = binomial)
 
 f <- function(x) predict(x, type = "link", newdata = mtcars)
-p <- hypotheses(mod, FUN = f)
+p <- hypotheses(mod, hypoth_fun = f)
 expect_inherits(p, "data.frame")
 expect_true(all(p$std.error > 0))
 
 f <- function(x) predict(x, type = "response", newdata = mtcars)
-p <- hypotheses(mod, FUN = f)
+p <- hypotheses(mod, hypoth_fun = f)
 expect_inherits(p, "data.frame")
 expect_true(all(p$std.error > 0))
 
 # equality between predictions: 1 and 2 equal, 2 and 3 different
 f <- function(x) predict(x, type = "link", newdata = mtcars)
-dmm <- hypotheses(mod, FUN = f, hypothesis = "b1 = b2")
+dmm <- hypotheses(mod, hypoth_fun = f, hypothesis = "b1 = b2")
 expect_equivalent(dmm$estimate, 0)
-dmm <- hypotheses(mod, FUN = f, hypothesis = "b3 = b2")
+dmm <- hypotheses(mod, hypoth_fun = f, hypothesis = "b3 = b2")
 expect_equivalent(dmm$estimate, 1.33154848763268)
 
 # named matrix
