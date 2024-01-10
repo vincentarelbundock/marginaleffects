34ba6a53ca641653289cfad3068e01c732de484a
Kevin Kunzmann <kevin.kunzmann@boehringer-ingelheim.com>
2024-01-10 11:04:29 +0000

    initial attempt to implement interface

---
+++ R/methods_mmrm.R
@@ -0,0 +1,36 @@
+#' @include set_coef.R
+#' @rdname set_coef
+#' @keywords internal
+#' @export
+set_coef.mmrm <- function(model, coefs, ...) {
+  model$beta_est  <- coefs
+  return(model)
+}
+
+
+#' @include get_coef.R
+#' @rdname get_coef
+#' @keywords internal
+#' @export
+get_coef.mmrm <- function(model, ...) {
+  return(coef(model, ...))
+}
+
+
+#' @include get_vcov.R
+#' @rdname get_vcov
+#' @keywords internal
+#' @export
+get_vcov.mmrm <- function(model, ...) {
+  return(vcov(model, ...))
+}
+
+
+#' @rdname get_predict
+#' @keywords internal
+#' @export
+get_predict.mmrm <- function(model, newdata = model$data, type = "response", ...) {
+  type <- match.arg(type)
+  res <- predict(model, newdata = newdata, ...)
+  return(res)
+}
