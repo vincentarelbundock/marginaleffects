1f25d178ba124b7f5cc5a08ca7876205cb576646
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-02-16 07:33:33 -0500

    rbuildignore

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,7 +1,5 @@
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
-    isTRUE(Sys.info()["sysname"] != "Windows") &&
-    # do not run test when checking package because .Rbuildignore excludes the modelarchive directory
-    dir.exists("inst/tinytest/modelarchive")) {
+    isTRUE(Sys.info()["sysname"] != "Windows")) {
     tinytest::test_package("marginaleffects")
 }
