678d09f0eb8741467c6576c5874da1709026029a
Noah Greifer <noah.greifer@gmail.com>
2024-05-06 16:17:53 -0400

    Fix for survreg objects

--- R/methods_survival.R
+++ R/methods_survival.R
@@ -1,3 +1,23 @@
+#' @include set_coef.R
+#' @rdname set_coef
+#' @export
+set_coef.survreg <- function(model, coefs, ...) {
+  #Reverse engineering insight::get_get_parameters.survreg(),
+  #which uses summary.survreg()
+
+  nvar0 <- length(model$coefficients)
+  nvar <- nrow(model$var)
+  if (nvar > nvar0) {
+    model[["coefficients"]][] <- coefs[-nvar0]
+    model[["scale"]][] <- exp(coefs[nvar0])
+  }
+  else {
+    model$coefficients[] <- coefs
+  }
+
+  model
+}
+
 #' @rdname get_predict
 #' @export
 get_predict.coxph <- function(model,
