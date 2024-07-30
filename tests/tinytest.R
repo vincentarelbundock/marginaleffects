132eb2b88c7b9fe1c979a0aa050d2afa3d1f0fc8
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-30 16:17:59 -0400

    error

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
-warning(isTRUE(Sys.getenv("R_NOT_CRAN") == "true"))
-warning(requireNamespace("tinytest", quietly = TRUE))
-warning(dir.exists("inst/tinytest"))
+if (!isTRUE(Sys.getenv("R_NOT_CRAN") == "true")) stop("R_NOT_CRAN")
+if (!requireNamespace("tinytest", quietly = TRUE)) stop("tinytest package")
+if (!dir.exists("inst/tinytest")) stop("directory")
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
     dir.exists("inst/tinytest")) {
