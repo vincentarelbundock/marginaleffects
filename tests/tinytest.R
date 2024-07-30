e977e05a9501b50dc07cbcad9a6458ceed20986e
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-30 16:30:45 -0400

    ci again

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
-if (!(dir.exists("inst/tinytest") || dir.exists("../inst/tinytest"))) stop(getwd())
+if (!(dir.exists(here::here("inst/tinytest")))) stop(getwd())
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
-    (dir.exists("inst/tinytest") || dir.exists("../inst/tinytest"))) {
+    dir.exists(here::here("inst/tinytest"))) {
     tinytest::test_package("marginaleffects")
 }
