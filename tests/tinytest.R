fd080b320ee231f4d9554f97b52b9dba3d6e6ce6
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-30 16:12:54 -0400

    warnings

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,8 @@
+warning(isTRUE(Sys.getenv("R_NOT_CRAN") == "true"))
+warning(requireNamespace("tinytest", quietly = TRUE))
+warning(dir.exists("inst/tinytest"))
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
     dir.exists("inst/tinytest")) {
-    stop("intentional breakage")
     tinytest::test_package("marginaleffects")
 }
