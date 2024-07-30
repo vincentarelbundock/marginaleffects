17877ec33b8d1c20a1525ed15ea3515c3cecad59
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-30 16:08:07 -0400

    intentional breakage 2

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
-    stop("intentional breakage")
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
     dir.exists("inst/tinytest")) {
+    stop("intentional breakage")
     tinytest::test_package("marginaleffects")
 }
