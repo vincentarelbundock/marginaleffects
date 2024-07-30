9a890ef224af750a17f5155a3c64a0045ded4caf
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-30 16:43:04 -0400

    minor

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,3 @@
-if (!(dir.exists(here::here("inst/tinytest")))) stop(getwd())
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
     dir.exists(here::here("inst/tinytest"))) {
