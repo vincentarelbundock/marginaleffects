31363a8c11dc1c2713069f0bdc8423a6d82fd8eb
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-08-02 08:23:39 -0400

    ci experiments

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,5 @@
 if (requireNamespace("tinytest", quietly = TRUE) &&
     isTRUE(Sys.getenv("R_NOT_CRAN") == "true") &&
-    !any(grepl("tinytest", readLines(".Rbuildignore")))) {
+    !any(grepl("tinytest", readLines(here::here(".Rbuildignore"))))) {
     tinytest::test_package("marginaleffects")
 }
