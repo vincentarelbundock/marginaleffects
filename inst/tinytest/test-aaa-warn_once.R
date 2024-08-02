58df58c924feb72858974834b33471bb1b20b549
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-08-02 08:32:02 -0400

    no intentional breakage

--- inst/tinytest/test-aaa-warn_once.R
+++ inst/tinytest/test-aaa-warn_once.R
@@ -1,8 +1,6 @@
 source("helpers.R")
 requiet("marginaleffects")
 
-expect_false(TRUE) # intentional breakage to test CI
-
 # factor in formula
 mod <- lm(mpg ~ hp + factor(cyl), data = mtcars)
 expect_warning(slopes(mod))
