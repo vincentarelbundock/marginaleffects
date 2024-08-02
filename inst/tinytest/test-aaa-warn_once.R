3a9a0a32c85be8ca59411b216b6c679a34af29d5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-08-02 07:50:20 -0400

    rbuildignor

--- inst/tinytest/test-aaa-warn_once.R
+++ inst/tinytest/test-aaa-warn_once.R
@@ -1,6 +1,8 @@
 source("helpers.R")
 requiet("marginaleffects")
 
+expect_false(TRUE) # intentional breakage to test CI
+
 # factor in formula
 mod <- lm(mpg ~ hp + factor(cyl), data = mtcars)
 expect_warning(slopes(mod))
