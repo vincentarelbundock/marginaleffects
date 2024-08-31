6002c88f500e0d17eece71ab6942f67ce440594e
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-08-30 21:59:18 -0400

    test fixup

--- inst/tinytest/test-pkg-glmtoolbox.R
+++ inst/tinytest/test-pkg-glmtoolbox.R
@@ -6,9 +6,9 @@ data(spruces)
 mod <- size ~ poly(days,4) + treat
 fit <- glmgee(mod, id=tree, family=Gamma(log), corstr="AR-M-dependent(1)", data=spruces)
 
-s <- avg_slopes(fit, newdata = spruces)
+s <- avg_slopes(fit)
 expect_inherits(s, "slopes")
-p <- avg_predictions(fit, newdata = spruces, by = "treat")
+p <- avg_predictions(fit, by = "treat")
 expect_inherits(p, "predictions")
-k <- avg_comparisons(fit, newdata = spruces)
+k <- avg_comparisons(fit)
 expect_inherits(k, "comparisons")
