91cd38f6aa693e55ba5e73193ca390c849e1255b
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-10-21 18:35:39 +0200

    WIP: add benchmark workflow (#1229)
    * init
    
    * minor
    
    * trigger on push
    
    * remove push event
    
    * do not run on all changes [skip ci]
    
    * try fix
    
    * try fix
    
    * try avoid installing from source
    
    * more
    
    * also install pkg-config
    
    * more
    
    * try with USE_SYSTEM_LIBGIT2
    
    * try with older version of git2r
    
    * add versions?
    
    * typo
    
    * more
    
    * more
    
    * more
    
    * works?
    
    * need to install marginaleffects deps
    
    * try with cross + bench
    
    * typo
    
    * add concurrency
    
    * typo
    
    * install more packages
    
    * fix?
    
    * try
    
    * more
    
    * more
    
    * fix params name
    
    * comment out some stuff
    
    * add write permission
    
    * more
    
    * use pull_request_target
    
    * more
    
    * remove function

---
+++ .github/scripts/benchmark.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+# Stored in the Github actions workflow
+pr_number <- Sys.getenv("PR_NUMBER")
+
+out <- cross::run(
+  pkgs = c(
+    "marginaleffects",
+    "vincentarelbundock/marginaleffects",
+    paste0("vincentarelbundock/marginaleffects#", pr_number)
+  ),
+  ~ {
+    library(marginaleffects)
+    library(data.table)
+
+    bench::press(
+      N = c(10),
+      {
+        dat <- data.frame(matrix(rnorm(N * 26), ncol = 26))
+        mod <- lm(X1 ~ ., dat)
+        bench::mark(
+          # marginal effects at the mean; no standard error
+          slopes(mod, vcov = FALSE, newdata = "mean"),
+          # marginal effects at the mean
+          slopes(mod, newdata = "mean"),
+          # 1 variable; no standard error
+          slopes(mod, vcov = FALSE, variables = "X3"),
+          # 1 variable
+          slopes(mod, variables = "X3"),
+          # 26 variables; no standard error
+          slopes(mod, vcov = FALSE),
+          # 26 variables
+          marginaleffects::slopes(mod),
+          check = FALSE,
+          iterations = 5
+        )
+      }
+    )
+  }
+)
+
+unnested <- out |>
+  mutate(
+    pkg = case_match(
+      pkg,
+      "marginaleffects" ~ "CRAN",
+      "vincentarelbundock/marginaleffects" ~ "main",
+      paste0("vincentarelbundock/marginaleffects#", pr_number) ~ "PR"
+    )
+  ) |>
+  unnest(result) |>
+  mutate(expression = as.character(expression))
+
+plot_result <- ggplot(unnested, aes(N, median, color = pkg)) +
+  geom_line(aes(linetype = pkg)) +
+  geom_point() +
+  facet_wrap(~expression) +
+  theme_light() +
+  theme(
+    legend.position = "bottom"
+  )
+
+ggsave(
+  plot = plot_result,
+  ".github/scripts/benchmark_result.png"
+)
