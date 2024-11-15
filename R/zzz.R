3ae62a4f4ee1023f81606160ce2887b2c53110c0
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-14 23:18:16 -0500

    typo

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   msg <- c(
     "Please cite the software that makes your work possible.",
     'For bibliographic info, type: citation("package_name")',
-    "Silence this warning with: options(open_source_promo = FALSE)"
+    "Silence this message with: options(open_source_promo = FALSE)"
   )
   msg <- paste(msg, collapse = "\n")
   if (isTRUE(getOption("open_source_promo", TRUE))) {
