dbf2d197dd7d6cdf510e6b06070a7e4ba5abdc44
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-14 23:51:39 -0500

    global options

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onAttach <- function(lib, pkg) {
   msg <- c(
-    "Please cite the software that makes your work possible.",
-    'For bibliographic info, type: citation("package_name")',
-    "Silence this message with: options(open_source_promo = FALSE)"
+    "Please cite the software developers who make your work possible.",
+    'Bibliographic info: citation("package_name")',
+    "Silence this message: options(marginaleffects_startup_message = FALSE)"
   )
   msg <- paste(msg, collapse = "\n")
-  if (isTRUE(getOption("open_source_promo", TRUE))) {
+  if (isTRUE(getOption("marginaleffects_startup_message", TRUE))) {
     packageStartupMessage(msg)
   }
   invisible()
--- man-roxygen/options.R
+++ man-roxygen/options.R
@@ -1,18 +1,15 @@
 #' @section Global options:
-#' 
+#'
 #' The behavior of `marginaleffects` functions can be modified by setting global options.
-#' 
+#'
 #' Disable some safety checks and warnings:
-#' 
-#' ```r
-#' options(marginaleffects_safe = FALSE)
-#' ```
 #'
-#' Omit some columns from the printed output:
-#'
-#' ```r
-#' options(marginaleffects_print_omit = c("p.value", "s.value"))`
-#' ```
+#' * `options(marginaleffects_startup_message = FALSE)`
+#'   - Disable the startup message printed on `library(marginaleffects)`.
+#' * `options(marginaleffects_safe = FALSE)`
+#'   - Disable safety checks and warnings.
+#' * `options(marginaleffects_print_omit = c("p.value", "s.value"))`
+#'   - Omit some columns from the printed output.
 #'
 #' Enforce lean return objects, sans information about the original model and
 #' data, and other ancillary attributes. Note that this will disable some
