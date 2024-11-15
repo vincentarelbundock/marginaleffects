dfc9bcab440265d364e9ea5306d7dc02c536647d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-15 06:43:26 -0500

    spacing

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,13 @@
 .onAttach <- function(lib, pkg) {
   msg <- c(
+    "",
     "Please cite the software developers who make your work possible.",
     "",
     'One package:             citation("package_name")',
     "All project packages:    softbib::softbib()",
     "",
-    "Silence this message: options(marginaleffects_startup_message = FALSE)"
+    "Silence this message: options(marginaleffects_startup_message = FALSE)",
+    ""
   )
   msg <- paste(msg, collapse = "\n")
   if (interactive() && isTRUE(getOption("marginaleffects_startup_message", TRUE))) {
