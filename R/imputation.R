0d0c75f1afabee069a781155817c255c680e08d2
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-16 12:30:04 -0500

    issue #1269 mice transform should be applied after pooling

--- R/imputation.R
+++ R/imputation.R
@@ -1,49 +1,56 @@
 process_imputation <- function(x, call_attr, marginal_means = FALSE) {
-    insight::check_if_installed("mice")
+  insight::check_if_installed("mice")
 
-    if (inherits(x, "mira")) {
-      x <- x$analyses
-    } else if (inherits(x, "amest")) {
-      x <- x
+  # issue #1269: transforms must be apply after pooling
+  tr <- call_attr[["transform"]]
+  call_attr[["transform"]] <- NULL
+
+  if (inherits(x, "mira")) {
+    x <- x$analyses
+  } else if (inherits(x, "amest")) {
+    x <- x
+  }
+
+  mfx_list <- vector("list", length(x))
+  for (i in seq_along(x)) {
+    calltmp <- call_attr
+    calltmp[["model"]] <- x[[i]]
+
+    # not sure why but this breaks marginal_means on "modeldata specified twice"
+    if (isFALSE(marginal_means)) {
+      calltmp[["modeldata"]] <- get_modeldata(x[[i]], additional_variables = FALSE)
     }
 
-    mfx_list <- vector("list", length(x))
-    for (i in seq_along(x)) {
-        calltmp <- call_attr
-        calltmp[["model"]] <- x[[i]]
+    mfx_list[[i]] <- evalup(calltmp)
+    if (i == 1) {
+      out <- mfx_list[[1]]
+    }
+    mfx_list[[i]]$term <- seq_len(nrow(mfx_list[[i]]))
+    class(mfx_list[[i]]) <- c("marginaleffects_mids", class(mfx_list[[i]]))
+  }
+  mipool <- mice::pool(mfx_list)
+  for (col in c("estimate", "statistic", "p.value", "conf.low", "conf.high")) {
+    if (col %in% colnames(out) && col %in% colnames(mipool$pooled)) {
+      out[[col]] <- mipool$pooled[[col]]
+    } else {
+      out[[col]] <- NULL
+    }
+  }
+  if ("df" %in% colnames(mipool$pooled)) {
+    out$df <- mipool$pooled$df
+  }
+  out$std.error <- sqrt(mipool$pooled$t)
+  out <- get_ci(
+    out,
+    vcov = call_attr[["vcov"]],
+    conf_level = call_attr[["conf_level"]],
+    df = mipool$pooled$df)
 
-        # not sure why but this breaks marginal_means on "modeldata specified twice"
-        if (isFALSE(marginal_means)) {
-            calltmp[["modeldata"]] <- get_modeldata( x[[i]], additional_variables = FALSE)
-        }
+  out <- backtransform(out, sanitize_transform(tr))
 
-        mfx_list[[i]] <- evalup(calltmp)
-        if (i == 1) {
-            out <- mfx_list[[1]]
-        }
-        mfx_list[[i]]$term <- seq_len(nrow(mfx_list[[i]]))
-        class(mfx_list[[i]]) <- c("marginaleffects_mids", class(mfx_list[[i]]))
-    }
-    mipool <- mice::pool(mfx_list)
-    for (col in c("estimate", "statistic", "p.value", "conf.low", "conf.high")) {
-        if (col %in% colnames(out) && col %in% colnames(mipool$pooled)) {
-            out[[col]] <- mipool$pooled[[col]]
-        } else {
-            out[[col]] <- NULL
-        }
-    }
-    if ("df" %in% colnames(mipool$pooled)) {
-        out$df <- mipool$pooled$df
-    }
-    out$std.error <- sqrt(mipool$pooled$t)
-    out <- get_ci(
-        out,
-        vcov = call_attr[["vcov"]],
-        conf_level = call_attr[["conf_level"]],
-        df = mipool$pooled$df)
-    attr(out, "inferences") <- mipool
-    attr(out, "model") <- mice::pool(lapply(mfx_list, attr, "model"))
-    return(out)
+  attr(out, "inferences") <- mipool
+  attr(out, "model") <- mice::pool(lapply(mfx_list, attr, "model"))
+  return(out)
 }
 
 
@@ -52,12 +59,12 @@ process_imputation <- function(x, call_attr, marginal_means = FALSE) {
 #' @noRd
 #' @export
 tidy.marginaleffects_mids <- function(x, ...) {
-    if (!"std.error" %in% colnames(x)) {
-        insight::format_error('The output does not include a `std.error` column. Some models do not generate standard errors when estimates are backtransformed (e.g., GLM models). One solution is to use `type="response"` for those models.')
-    }
-    out <- as.data.frame(x[, c("estimate", "std.error")])
-    out$term <- seq_len(nrow(out))
-    return(out)
+  if (!"std.error" %in% colnames(x)) {
+    insight::format_error('The output does not include a `std.error` column. Some models do not generate standard errors when estimates are backtransformed (e.g., GLM models). One solution is to use `type="response"` for those models.')
+  }
+  out <- as.data.frame(x[, c("estimate", "std.error")])
+  out$term <- seq_len(nrow(out))
+  return(out)
 }
 
 
@@ -66,5 +73,5 @@ tidy.marginaleffects_mids <- function(x, ...) {
 #' @noRd
 #' @export
 glance.marginaleffects_mids <- function(x, ...) {
-    data.frame()
+  data.frame()
 }
