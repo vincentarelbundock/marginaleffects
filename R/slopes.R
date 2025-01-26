b5ba57ac4d4e143ccd562914befb912b66c33338
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-01-26 07:11:03 -0500

    urls

--- R/slopes.R
+++ R/slopes.R
@@ -10,8 +10,8 @@
 #'
 #' See the slopes vignette and package website for worked examples and case studies:
 #'
-#' * <https://marginaleffects.com/chapters/slopes.html>
-#' * <https://marginaleffects.com/>
+#' * [https://marginaleffects.com/chapters/slopes.html](https://marginaleffects.com/chapters/slopes.html)
+#' * [https://marginaleffects.com/](https://marginaleffects.com/)
 #'
 #' @details
 #' A "slope" or "marginal effect" is the partial derivative of the regression equation
@@ -105,7 +105,7 @@
 #'   - Returns a data frame with columns `term` and `estimate` (mandatory) and `rowid` (optional).
 #'   - The function can also accept optional input arguments: `newdata`, `by`, `draws`.
 #'   - This function approach will not work for Bayesian models or with bootstrapping. In those cases, it is easy to use `get_draws()` to extract and manipulate the draws directly.
-#' + See the Examples section below and the vignette: https://marginaleffects.com/chapters/hypothesis.html
+#' + See the Examples section below and the vignette: [https://marginaleffects.com/chapters/hypothesis.html](https://marginaleffects.com/chapters/hypothesis.html)
 #' @param df Degrees of freedom used to compute p values and confidence intervals. A single numeric value between 1 and `Inf`. When `df` is `Inf`, the normal distribution is used. When `df` is finite, the `t` distribution is used. See [insight::get_df] for a convenient function to extract degrees of freedom. Ex: `slopes(model, df = insight::get_df(model))`
 #' @param eps NULL or numeric value which determines the step size to use when
 #' calculating numerical derivatives: (f(x+eps)-f(x))/eps. When `eps` is
