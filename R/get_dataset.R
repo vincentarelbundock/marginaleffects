c76c05df12c3c43d840dca49f5876268cfd74c5f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-02-06 07:54:45 -0500

    get_dataset: more datasets

--- R/get_dataset.R
+++ R/get_dataset.R
@@ -6,7 +6,7 @@
 #' [https://vincentarelbundock.github.io/Rdatasets/](https://vincentarelbundock.github.io/Rdatasets/)
 #'
 #' @param dataset String. Name of the dataset to download.
-#'   - `marginaleffects` archive: `affairs`, `airbnb`, `immigration`, `lottery`, `military`, `thornton`
+#'   - `marginaleffects` archive: `affairs`, `airbnb`, `ces_demographics`, `ces_survey`, `immigration`, `lottery`, `military`, `thornton`, `factorial_01`, `interaction_01`, `interaction_02`, `interaction_03`, `interaction_04`, `polynomial_01`, `polynomial_02`
 #'   - Rdatasets archive: The name of a dataset listed on the Rdatasets index. See the website or the `search` argument.
 #' @param package String. Package name that originally published the data.
 #' @param docs Logical. If TRUE open the documentation using `getOption("viewer")` or the Rstudio viewer.
@@ -51,28 +51,38 @@ get_dataset <- function(
     if (identical(package, "marginaleffects")) {
         insight::check_if_installed("nanoparquet")
 
-        checkmate::assert_choice(dataset, c("affairs", "airbnb", "immigration", "lottery", "military", "thornton"))
-
-        data <- switch(dataset,
+        data_dict <- c(
             "affairs" = "https://marginaleffects.com/data/affairs.parquet",
             "airbnb" = "https://marginaleffects.com/data/airbnb.parquet",
+            "ces_demographics" = "https://marginaleffects.com/data/ces_demographics.parquet",
+            "ces_survey" = "https://marginaleffects.com/data/ces_survey.parquet",
             "immigration" = "https://marginaleffects.com/data/immigration.parquet",
             "lottery" = "https://marginaleffects.com/data/lottery.parquet",
             "military" = "https://marginaleffects.com/data/military.parquet",
-            "thornton" = "https://marginaleffects.com/data/thornton.parquet"
+            "thornton" = "https://marginaleffects.com/data/thornton.parquet",
+            "factorial_01" = "https://marginaleffects.com/data/factorial_01.parquet",
+            "interaction_01" = "https://marginaleffects.com/data/interaction_01.parquet",
+            "interaction_02" = "https://marginaleffects.com/data/interaction_02.parquet",
+            "interaction_03" = "https://marginaleffects.com/data/interaction_03.parquet",
+            "interaction_04" = "https://marginaleffects.com/data/interaction_04.parquet",
+            "polynomial_01" = "https://marginaleffects.com/data/polynomial_01.parquet",
+            "polynomial_02" = "https://marginaleffects.com/data/polynomial_02.parquet"
         )
+
+        checkmate::assert_choice(dataset, names(data_dict))
+
+        data <- data_dict[dataset]
+        if (grepl("factorial|interaction|polynomial", dataset)) {
+            documentation <- "https://marginaleffects.com/data/model_to_meaning_simulated_data.html"
+        } else if (grepl("^ces", dataset)) {
+            documentation <- "https://marginaleffects.com/data/ces.html"
+        } else {
+            documentation <- sub("parquet$", "html", data_url)
+        }
+
         temp_file <- tempfile(fileext = ".parquet")
         utils::download.file(data, temp_file, mode = "wb", quiet = TRUE)
         data <- nanoparquet::read_parquet(temp_file)
-
-        documentation <- switch(dataset,
-            "affairs" = "https://marginaleffects.com/data/affairs.html",
-            "airbnb" = "https://marginaleffects.com/data/airbnb.html",
-            "immigration" = "https://marginaleffects.com/data/immigration.html",
-            "lottery" = "https://marginaleffects.com/data/lottery.html",
-            "military" = "https://marginaleffects.com/data/military.html",
-            "thornton" = "https://marginaleffects.com/data/thornton.html"
-        )
 
         # Rdatasets
     } else {
