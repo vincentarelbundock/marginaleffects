1d408c2cfd025e4b0d26f16fa1e8485abdaefd89
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-03-29 08:42:31 -0400

    tinytest

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,7 @@
 NOT_CRAN <- isTRUE(Sys.getenv("R_NOT_CRAN") == "true")
 TINYTEST <- requireNamespace("tinytest", quietly = TRUE)
+LOCAL_TESTS <- dir.exists("inst/tinytest")
 
-if (TINYTEST && NOT_CRAN) {
+if (TINYTEST && NOT_CRAN && LOCAL_TESTS) {
     tinytest::test_package("marginaleffects")
 }
