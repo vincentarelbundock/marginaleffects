06ca16fe55e498b5f5b55c1a24285c794f439b4c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-05-19 09:45:32 -0400

    workflow

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
 run <- FALSE
 pkg <- requireNamespace("tinytest", quietly = TRUE)
-home <- grepl("(?i)vince", Sys.info()["nodename"]) || NOT_CRAN
+home <- grepl("(?i)vince", Sys.info()["nodename"]) || identical(Sys.getenv("R_NOT_CRAN"), "true")
 if (pkg && home && run) {
     tinytest::test_package("marginaleffects")
 }
