bf780a28adb75e74507622ae2af71c4af3e950eb
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-13 20:41:36 -0400

    simple: get_modeldata()

--- inst/tinytest/test-datagrid.R
+++ inst/tinytest/test-datagrid.R
@@ -69,7 +69,7 @@ expect_error(
 
 
 # Issue #688
-dat <<- transform(mtcars, cyl = factor(cyl))
+dat <- transform(mtcars, cyl = factor(cyl))
 mod <- lm(mpg ~ hp, data = dat)
 d <- datagrid(model = mod, by = c("carb", "cyl"), response = TRUE)
 k <- aggregate(cbind(mpg, hp) ~ carb + cyl, data = dat, FUN = mean)
@@ -78,7 +78,7 @@ expect_equivalent(k$mpg, d$mpg)
 
 # Issue 766: categorical predictors + variables arg + avg
 requiet("Matchit")
-data('lalonde', package = 'MatchIt')
+data("lalonde", package = "MatchIt")
 fit <- lm(re78 ~ race * treat, data = lalonde)
 
 a = predict(fit, branewdata = lalonde)
@@ -114,7 +114,7 @@ expect_equivalent(a, b$estimate)
 
 # Issue #1058:  Missing attributes for marginaleffects::datagrid(..., by = ) #1058
 tmp <- mtcars
-tmp <- tmp[c('mpg', 'cyl', 'hp')]
+tmp <- tmp[, c("mpg", "cyl", "hp")]
 tmp$cyl <- as.factor(tmp$cyl)
 tmp$hp <- as.factor(tmp$hp)
 at1 <- attributes(datagrid(newdata = tmp, by = "cyl", hp = unique))
