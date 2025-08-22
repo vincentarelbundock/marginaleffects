a9489509e1363339d75ceefd84cb5272dfeb5a75
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-22 11:16:34 -0400

    test

--- inst/tinytest/test-rope.R
+++ inst/tinytest/test-rope.R
@@ -4,7 +4,7 @@ requiet("bayestestR")
 
 
 dat <- transform(mtcars, cyl = factor(cyl))
-mod <- stan_glm(mpg ~ hp + cyl, data = mtcars)
+void <- capture.output(mod <- stan_glm(mpg ~ hp + cyl, data = mtcars))
 
 lo <- 21
 hi <- 26
