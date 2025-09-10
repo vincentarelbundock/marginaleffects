fcf74b4cea34b5c01603e54787e2102a3356ee1e
etiennebacher <etienne.bacher@protonmail.com>
2025-09-10 11:40:29 +0200

    simplify util test

--- inst/tinytest/test-utils.R
+++ inst/tinytest/test-utils.R
@@ -5,44 +5,44 @@ using("marginaleffects")
 
 expect_equivalent(
     marginaleffects:::is_binary(1:10),
-    isTRUE(all(1:10 %in% 0:1))
+    FALSE
 )
 
 expect_equivalent(
     marginaleffects:::is_binary(0:1),
-    isTRUE(all(0:1 %in% 0:1))
+    TRUE
 )
 
 expect_equivalent(
     marginaleffects:::is_binary(c(0, 0.5, 1)),
-    isTRUE(all(c(0, 0.5, 1) %in% 0:1))
+    FALSE
 )
 
 # with single values
 
 expect_equivalent(
     marginaleffects:::is_binary(1),
-    isTRUE(all(1 %in% 0:1))
+    TRUE
 )
 
 expect_equivalent(
     marginaleffects:::is_binary(2),
-    isTRUE(all(2 %in% 0:1))
+    FALSE
 )
 
 # with missings / NULL
 
 expect_equivalent(
     marginaleffects:::is_binary(c(0, 0.5, NA, 1)),
-    isTRUE(all(c(0, 0.5, NA, 1) %in% 0:1))
+    FALSE
 )
 
 expect_equivalent(
     marginaleffects:::is_binary(NA),
-    isTRUE(all(NA %in% 0:1))
+    FALSE
 )
 
 expect_equivalent(
     marginaleffects:::is_binary(NULL),
-    isTRUE(all(NULL %in% 0:1))
+    TRUE
 )
