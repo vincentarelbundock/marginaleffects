731b71bc227d2b4ae0ddde237766ab0cfa36e996
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2026-02-21 10:24:46 -0500

    Issue #1672 duplicated method

--- R/methods_plm.R
+++ R/methods_plm.R
@@ -2,18 +2,6 @@
 #' @rdname sanitize_model_specific
 #' @keywords internal
 sanitize_model_specific.plm <- function(model, ...) {
-    if ("within" %in% model$args$model) {
-        stop(
-            'The `plm::predict` function does not appear to support the `newdata` argument when `plm(model="within")`. Therefore, `marginaleffects` cannot support "within" models, even if it supports many other models produced by the `plm` package. You may want to try the `fixest` package instead.',
-            call. = FALSE
-        )
-    }
-    return(model)
-}
-
-
-#' @rdname sanitize_model_specific
-sanitize_model_specific.plm <- function(model, ...) {
     insight::check_if_installed("insight", minimum_version = "0.17.1")
     return(model)
 }
